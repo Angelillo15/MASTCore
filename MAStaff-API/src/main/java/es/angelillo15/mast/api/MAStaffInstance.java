2a5aa08053115850e43a878ea0ecad5dd0f3f64a
Angelillo15 <angelrizosrubios@gmail.com>
2022-12-25 14:02:45 +0100

    Added new Exceptions

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/MAStaffInstance.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/MAStaffInstance.java
@@ -1,4 +1,21 @@
 package es.angelillo15.mast.api;
 
+import es.angelillo15.mast.api.exceptions.PluginNotLoadedException;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+
 public interface MAStaffInstance {
+    /**
+     * @return the plugin instance
+     *
+     */
+    public static MAStaffInstance getInstance(){
+        MAStaffInstance plugin = (MAStaffInstance) Bukkit.getPluginManager().getPlugin("MAStaff");
+        if(plugin == null){
+            throw new PluginNotLoadedException("MAStaff plugin is not loaded");
+        } else {
+            return plugin;
+        }
+    }
+
 }
---
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/package-info.java
@@ -0,0 +1,9 @@
+package es.angelillo15.mast.api;
+/**
+ * MAStaff API
+ *
+ * @author Angelillo15
+ * @since 1.0
+ * @see es.angelillo15.mast.api.MAStaffInstance
+ */
+
