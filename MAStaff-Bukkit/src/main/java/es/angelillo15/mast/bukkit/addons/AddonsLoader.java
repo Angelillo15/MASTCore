061c088b253389a0ebc67d48927f985b3c7e615a
Angelillo15 <angelrizosrubios@gmail.com>
2023-02-04 14:00:32 +0100

    Updates in AddonLoader

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/addons/AddonsLoader.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/addons/AddonsLoader.java
@@ -7,21 +7,25 @@ import es.angelillo15.mast.bukkit.MAStaff;
 import lombok.SneakyThrows;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Properties;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class AddonsLoader {
     @SneakyThrows
-    public void loadAddons() {
+    public static void loadAddons() {
+        MAStaff.getPlugin().getPLogger().debug("Loading addons...");
         File addonsFolder = new File(MAStaff.getPlugin().getDataFolder() + File.separator + "addons");
         if (!addonsFolder.exists()) {
             addonsFolder.mkdir();
         }
 
         for (File file : addonsFolder.listFiles()) {
-            if(file.isDirectory()) return;
+            MAStaff.getPlugin().getPLogger().debug("Loading addon " + file.getName() + "...");
             if(!file.getName().endsWith(".jar")) return;
             URL[] urls = new URL[]{file.toURI().toURL()};
             AddonDescription addonDescription = new AddonDescription();
@@ -29,22 +33,52 @@ public class AddonsLoader {
             if(file.getName().contains(" ")) return;
             JarFile jarFile = new JarFile(file);
             Properties properties = new Properties();
+            InputStream propertiesFile;
 
-            properties.load(jarFile.getInputStream(jarFile.getJarEntry("addon.properties")));
+            JarEntry jarEntry = jarFile.getJarEntry("addon.properties");
+
+            if (jarEntry == null) {
+                MAStaff.getPlugin().getPLogger().error("Addon " + file.getName() + " doesn't have a addon.properties file!");
+                return;
+            }
+
+            propertiesFile = jarFile.getInputStream(jarEntry);
+
+            if (propertiesFile == null) {
+                MAStaff.getPlugin().getPLogger().error("Addon " + file.getName() + " doesn't have a addon.properties file!");
+                return;
+            }
+
+            properties.load(propertiesFile);
 
             addonDescription.setName(properties.getProperty("name"));
             addonDescription.setMain(properties.getProperty("main"));
             addonDescription.setVersion(properties.getProperty("version"));
             addonDescription.setAuthor(properties.getProperty("author"));
 
-            Class cls = new URLClassLoader(urls).loadClass(addonDescription.getMain());
+            Class cls = new URLClassLoader(urls, MAStaff.getPlugin().getClass().getClassLoader())
+                    .loadClass(addonDescription.getMain());
 
             MAStaffAddon addon = (MAStaffAddon) cls.newInstance();
 
-            addon.init(new File(file + File.separator + addonDescription.getName()), addonDescription, MAStaff.getPlugin());
+            addon.init(new File(file.getParentFile() + File.separator + addonDescription.getName()), addonDescription, MAStaff.getPlugin());
             addon.onEnable();
 
             AddonsManager.registerAddon(addon);
+            MAStaff.getPlugin().getPLogger().debug("Addon " + addonDescription.getName() + " Version: " + addonDescription.getVersion() + " loaded!");
+
         }
+        MAStaff.getPlugin().getPLogger().debug("Addons loaded!");
+    }
+
+    @SneakyThrows
+    public static void disableAddons(){
+        MAStaff.getPlugin().getPLogger().debug("Disabling addons...");
+        for(MAStaffAddon addon : AddonsManager.getAddons().values()){
+            MAStaff.getPlugin().getPLogger().debug("Disabling addon " + addon.getDescriptionFile().getName() + "...");
+            addon.onDisable();
+            MAStaff.getPlugin().getPLogger().debug("Addon " + addon.getDescriptionFile().getName() + " disabled!");
+        }
+        MAStaff.getPlugin().getPLogger().debug("Addons disabled!");
     }
 }
