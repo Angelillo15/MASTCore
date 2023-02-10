1e1fc8990912960c30323f9c9b4aaae3f3333f37
Angelillo15 <contact@angelillo15.es>
2023-02-10 21:59:16 +0100

    Added constants java

---
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/Constants.java
@@ -0,0 +1,15 @@
+package es.angelillo15.mast.api;
+
+public class Constants {
+    public static final String VERSION = "{version}";
+
+    public static final String COMMIT = "{git-commit}";
+
+    public static final String COMMIT_USER = "{git-user}";
+
+    public static final String COMMIT_TIME = "{git-date}";
+
+    public static final String GIT_BRANCH = "{git-branch}";
+
+    private Constants() {}
+}
