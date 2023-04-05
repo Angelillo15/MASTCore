e2d75f6015c3e297d28955f6a19a80dcc250e193
Angelillo15 <contact@angelillo15.es>
2023-04-05 21:23:51 +0200

    Added new event todo

---
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/listener/staffmode/achivement/OnAchievement.java
@@ -0,0 +1,15 @@
+package es.angelillo15.mast.bukkit.listener.staffmode.achivement;
+
+import es.angelillo15.mast.api.IStaffPlayer;
+import es.angelillo15.mast.api.managers.StaffPlayersManagers;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+
+public class OnAchievement implements Listener {
+    @EventHandler
+    public void onAchievement(PlayerAdvancementDoneEvent event){
+        // todo implement paper api
+    }
+}
