afda124b95006b055893f3d8d8286ea220dd7e7c
Angelillo15 <contact@angelillo15.es>
2023-04-07 14:41:04 +0200

    use new clas for manage PreviousServerData

--- MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/listener/StaffJoinChange.java
+++ MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/listener/StaffJoinChange.java
@@ -3,8 +3,8 @@ package es.angelillo15.mast.bungee.listener;
 import es.angelillo15.mast.api.event.bungee.staff.StaffJoinEvent;
 import es.angelillo15.mast.api.event.bungee.staff.StaffLeaveEvent;
 import es.angelillo15.mast.api.event.bungee.staff.StaffSwitchServerEvent;
-import es.angelillo15.mast.bungee.MASTBungeeManager;
 import es.angelillo15.mast.bungee.config.Messages;
+import es.angelillo15.mast.bungee.utils.PreviousServerManager;
 import es.angelillo15.mast.bungee.utils.StaffUtils;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -18,11 +18,11 @@ public class StaffJoinChange implements Listener {
     @EventHandler
     public void onJoin(PostLoginEvent event) {
         ProxiedPlayer player = event.getPlayer();
-        if(player.hasPermission("mast.staff.join")){
+        if (player.hasPermission("mast.staff.join")) {
             StaffUtils.sendBroadcastPermissionMessage(Messages.getStaffJoin()
-                    .replace("{player}", player.getName())
-                    .replace("{server}", "Proxy")
-            , "mast.staff.notify.join");
+                            .replace("{player}", player.getName())
+                            .replace("{server}", "Proxy")
+                    , "mast.staff.notify.join");
 
             ProxyServer.getInstance().getPluginManager().callEvent(new StaffJoinEvent(player));
         }
@@ -37,30 +37,26 @@ public class StaffJoinChange implements Listener {
         String fromServer;
         if (player.hasPermission("mast.staff.change")) {
             fromServer = "Proxy";
-            if (!MASTBungeeManager.getInstance().getPreviousServer().containsKey(player.getUniqueId())) {
-                MASTBungeeManager.getInstance().setPreviousServer(player.getUniqueId(), "Proxy");
-            } else {
-                fromServer = MASTBungeeManager.getInstance().getPreviousServer(player.getUniqueId());
-            }
 
+            if (PreviousServerManager.hasPreviousServer(player.getUniqueId())) {
+                fromServer = PreviousServerManager.getPreviousServer(player.getUniqueId());
+            } else {
+                PreviousServerManager.setPreviousServer(player.getUniqueId(), "Proxy");
+            }
 
             StaffUtils.sendBroadcastPermissionMessage(Messages.getStaffChangeServer()
                             .replace("{player}", player.getDisplayName())
                             .replace("{server}", player.getServer().getInfo().getName())
-                            .replace("{fromServer}", MASTBungeeManager.getInstance().getPreviousServer(player.getUniqueId())),
+                            .replace("{fromServer}", PreviousServerManager.getPreviousServer(player.getUniqueId())),
                     "mast.staff.notify.change");
 
             ProxyServer.getInstance().getPluginManager().callEvent(new StaffSwitchServerEvent(event.getPlayer(),
                     fromServer,
                     player.getServer().getInfo().getName())
             );
-
-
         }
 
-
-
-        MASTBungeeManager.getInstance().setPreviousServer(player.getUniqueId(), player.getServer().getInfo().getName());
+        PreviousServerManager.setPreviousServer(player.getUniqueId(), player.getServer().getInfo().getName());
     }
 
     @EventHandler
@@ -68,13 +64,13 @@ public class StaffJoinChange implements Listener {
         ProxiedPlayer player = event.getPlayer();
         if (player.hasPermission("mast.staff.leave")) {
             StaffUtils.sendBroadcastPermissionMessage(Messages.getStaffLeave()
-                    .replace("{player}", player.getDisplayName()),
+                            .replace("{player}", player.getDisplayName()),
                     "mast.staff.notify.leave");
 
         }
 
         ProxyServer.getInstance().getPluginManager().callEvent(new StaffLeaveEvent(player));
 
-        MASTBungeeManager.getInstance().removePreviousServer(player.getUniqueId());
+        PreviousServerManager.removePreviousServer(player.getUniqueId());
     }
 }
