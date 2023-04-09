f2ded20899dc6059d34e90198f0dda94fdef5833
Angelillo15 <contact@angelillo15.es>
2023-04-09 18:09:43 +0200

    Added Event logic to the subscriber

--- MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/manager/RedisSubscriber.java
+++ MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/manager/RedisSubscriber.java
@@ -1,8 +1,11 @@
 package es.angelillo15.mast.bungee.manager;
 
 import es.angelillo15.mast.api.managers.ManagerExecutor;
+import es.angelillo15.mast.api.redis.Event;
+import es.angelillo15.mast.api.redis.EventManager;
 import es.angelillo15.mast.bungee.MAStaff;
 import es.angelillo15.mast.bungee.config.Config;
+import lombok.SneakyThrows;
 import redis.clients.jedis.JedisPubSub;
 
 public class RedisSubscriber implements ManagerExecutor {
@@ -11,9 +14,28 @@ public class RedisSubscriber implements ManagerExecutor {
         MAStaff.getInstance().getPLogger().info("Subscribing to channel: " + Config.Redis.getChannel());
         new Thread(() -> {
             RedisManager.getJedisSubscriber().subscribe(new JedisPubSub() {
+                @SneakyThrows
                 @Override
                 public void onMessage(String channel, String message) {
-                    MAStaff.getInstance().getPLogger().info("Received message: " + message);
+                    if (message.startsWith(Config.Redis.getServerName() + ":")) {
+                        return;
+                    }
+
+                    MAStaff.getInstance().getPLogger().debug("Received message: " + message);
+
+                    String messageWithoutServerName = message.split(":")[1];
+
+                    if (!EventManager.getInstance().eventExists(messageWithoutServerName)) {
+                        return;
+                    }
+
+                    Event event = EventManager.getInstance().getEvent(messageWithoutServerName)
+                            .getDeclaredConstructor(String.class, String.class)
+                            .newInstance(messageWithoutServerName,
+                                    message.split(":")[0]
+                            );
+
+                    EventManager.getInstance().fireEvent(event);
                 }
             }, Config.Redis.getChannel());
 
