a6b5beeb44b0b3ce57dee5c536796760278c53e2
Angelillo15 <contact@angelillo15.es>
2023-04-14 23:50:24 +0200

    Added event class to the constructor with it respective getter

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/redis/Events.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/redis/Events.java
@@ -1,16 +1,25 @@
 package es.angelillo15.mast.api.redis;
 
+import es.angelillo15.mast.api.redis.events.server.ServerConnectedEvent;
+import es.angelillo15.mast.api.redis.events.server.ServerDisconnectedEvent;
+
 public enum Events {
-    SERVER_CONNECTED("ServerConnectedEvent"),
-    SERVER_DISCONNECTED("ServerDisconnectedEvent");
+    SERVER_CONNECTED("ServerConnectedEvent", ServerConnectedEvent.class),
+    SERVER_DISCONNECTED("ServerDisconnectedEvent", ServerDisconnectedEvent.class);
 
     private final String event;
+    private final Class<? extends Event> eventClass;
 
-    Events(String event) {
+    Events(String event, Class<? extends Event> eventClass) {
         this.event = event;
+        this.eventClass = eventClass;
     }
 
-    public String getEvent() {
+    public String getEventName() {
         return event;
+    }
+
+    public Class<? extends Event> getEvent() {
+        return eventClass;
     }
 }
