afc9cdc61f38b24ac8007bb222b79f180e97e38d
Angelillo15 <contact@angelillo15.es>
2023-04-14 19:15:14 +0200

    update constructor

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/redis/events/server/ServerConnectedEvent.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/redis/events/server/ServerConnectedEvent.java
@@ -1,9 +1,14 @@
 package es.angelillo15.mast.api.redis.events.server;
 
+import es.angelillo15.mast.api.redis.Events;
 import es.angelillo15.mast.api.redis.RedisEvent;
 
 public class ServerConnectedEvent extends RedisEvent {
     public ServerConnectedEvent(String message, String serverName) {
         super(message, serverName);
+    }
+
+    public ServerConnectedEvent(String serverName) {
+        super(Events.SERVER_CONNECTED.getEvent(), serverName);
     }
 }
