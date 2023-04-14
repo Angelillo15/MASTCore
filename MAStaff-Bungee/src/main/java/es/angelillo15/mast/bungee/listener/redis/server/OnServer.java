679d0751dce6107671f74a22703616e3020db1ff
Angelillo15 <contact@angelillo15.es>
2023-04-14 23:50:43 +0200

    Change message format

--- MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/listener/redis/server/OnServer.java
+++ MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/listener/redis/server/OnServer.java
@@ -1,18 +1,21 @@
 package es.angelillo15.mast.bungee.listener.redis.server;
 
+import es.angelillo15.mast.api.TextUtils;
 import es.angelillo15.mast.api.redis.EventHandler;
 import es.angelillo15.mast.api.redis.Listener;
 import es.angelillo15.mast.api.redis.events.server.ServerConnectedEvent;
 import es.angelillo15.mast.api.redis.events.server.ServerDisconnectedEvent;
+import es.angelillo15.mast.bungee.MAStaff;
+import es.angelillo15.mast.bungee.config.Messages;
 
 public class OnServer implements Listener {
     @EventHandler
     public void onServerConnectedEvent(ServerConnectedEvent event) {
-        System.out.println("Server connected: " + event.getServerName());
+        MAStaff.getInstance().getPLogger().info(TextUtils.simpleColorize(Messages.getPrefix() + " &rServer connected: &b" + event.getServerName()));
     }
 
     @EventHandler
     public void onServerDisconnectedEvent(ServerDisconnectedEvent event) {
-        System.out.println("Server disconnected: " + event.getServerName());
+        MAStaff.getInstance().getPLogger().info(TextUtils.simpleColorize(Messages.getPrefix() + " &rServer disconnected: &b" + event.getServerName()));
     }
 }
