7688aed1131bc0bd9e5dd5bf763dcb4b1a950d7b
Angelillo15 <contact@angelillo15.es>
2023-04-15 12:38:53 +0200

    Added StaffJoin and Leave Redis events

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/redis/Events.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/redis/Events.java
@@ -2,10 +2,14 @@ package es.angelillo15.mast.api.redis;
 
 import es.angelillo15.mast.api.redis.events.server.ServerConnectedEvent;
 import es.angelillo15.mast.api.redis.events.server.ServerDisconnectedEvent;
+import es.angelillo15.mast.api.redis.events.staff.StaffJoinEvent;
+import es.angelillo15.mast.api.redis.events.staff.StaffLeaveEvent;
 
 public enum Events {
     SERVER_CONNECTED("ServerConnectedEvent", ServerConnectedEvent.class),
-    SERVER_DISCONNECTED("ServerDisconnectedEvent", ServerDisconnectedEvent.class);
+    SERVER_DISCONNECTED("ServerDisconnectedEvent", ServerDisconnectedEvent.class),
+    STAFF_JOIN("StaffJoinEvent",StaffJoinEvent .class),
+    STAFF_LEAVE("StaffLeaveEvent", StaffLeaveEvent.class);
 
     private final String event;
     private final Class<? extends Event> eventClass;
---
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/redis/events/staff/StaffJoinEvent.java
@@ -0,0 +1,25 @@
+package es.angelillo15.mast.api.redis.events.staff;
+
+import es.angelillo15.mast.api.redis.Events;
+import es.angelillo15.mast.api.redis.RedisEvent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+public class StaffJoinEvent extends RedisEvent {
+    private String staffName;
+    public StaffJoinEvent(String message, String serverName) {
+        super(message, serverName);
+        this.staffName = message.split(":")[2];
+    }
+
+    public StaffJoinEvent(String serverName, ProxiedPlayer staff) {
+        super(Events.STAFF_JOIN.getEventName() + ":" + staff.getName(), serverName);
+    }
+
+    /**
+     * Get the name of the staff member who joined
+     * @return The name of the staff member who joined
+     */
+    public String getStaffName() {
+        return staffName;
+    }
+}
