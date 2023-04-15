eccce1d6641165f39ca4465d2080f588ec9c6e13
Angelillo15 <contact@angelillo15.es>
2023-04-15 14:00:11 +0200

    Convert message to base64 and then decode

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/redis/events/staff/StaffChatMessageEvent.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/redis/events/staff/StaffChatMessageEvent.java
@@ -2,30 +2,34 @@ package es.angelillo15.mast.api.redis.events.staff;
 
 import es.angelillo15.mast.api.redis.Events;
 import es.angelillo15.mast.api.redis.RedisEvent;
-
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class StaffChatMessageEvent extends RedisEvent {
     private String staffName;
-    private String message;
+    private String staffMessage;
     private String server;
 
     public StaffChatMessageEvent(String message, String serverName) {
         super(message, serverName);
-        String[] split = message.split(">&");
+        String[] split = message.split(">");
         this.staffName = split[1];
         this.server = split[2];
 
         if (split.length > 3) {
-            this.message = split[3];
+            this.staffMessage = new String(Base64.getDecoder().decode(split[3]), StandardCharsets.UTF_8);
         } else {
-            this.message = "";
+            this.staffMessage = "";
         }
     }
 
     public StaffChatMessageEvent(String serverName, String staffName, String message, String server) {
-        super(Events.STAFF_CHAT_MESSAGE.getEventName() + ">&" + staffName + ">&" + server + ">&" + message,  serverName);
+        // Encode to base64
+        super(Events.STAFF_CHAT_MESSAGE.getEventName() + ">" + staffName + ">" + server + ">" +
+                Base64.getEncoder().encodeToString(message.getBytes(StandardCharsets.UTF_8)),
+                serverName
+        );
+
     }
 
     /**
@@ -40,8 +44,8 @@ public class StaffChatMessageEvent extends RedisEvent {
      * Get the message from the event
      * @return The message
      */
-    public String getMessage() {
-        return message;
+    public String getStaffMessage() {
+        return staffMessage;
     }
 
     /**
