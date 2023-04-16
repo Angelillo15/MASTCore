b0023f89d25f366c8ca3d7ed80f8d91d4b1acf57
Angelillo15 <contact@angelillo15.es>
2023-04-16 14:37:20 +0200

    Added logic to disable staff mode on exit

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/listener/staffmode/OnJoinLeave.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/listener/staffmode/OnJoinLeave.java
@@ -3,10 +3,10 @@ package es.angelillo15.mast.bukkit.listener.staffmode;
 import es.angelillo15.mast.api.IStaffPlayer;
 import es.angelillo15.mast.api.Permissions;
 import es.angelillo15.mast.api.database.sql.CommonQueries;
-import es.angelillo15.mast.api.event.bukkit.staff.server.StaffJoinEvent;
-import es.angelillo15.mast.api.event.bukkit.staff.server.StaffLeaveEvent;
 import es.angelillo15.mast.api.managers.StaffPlayersManagers;
 import es.angelillo15.mast.bukkit.MAStaff;
+import es.angelillo15.mast.bukkit.StaffPlayer;
+import es.angelillo15.mast.bukkit.config.Config;
 import es.angelillo15.mast.bukkit.config.Messages;
 import es.angelillo15.mast.bukkit.utils.StaffUtils;
 import org.bukkit.GameMode;
@@ -55,12 +55,17 @@ public class OnJoinLeave implements Listener {
 
         //MAStaff.getPlugin().getServer().getPluginManager().callEvent(new StaffLeaveEvent(player));
 
-        IStaffPlayer staffPlayer = StaffPlayersManagers.getStaffPlayer(player);
+        StaffPlayer staffPlayer = (StaffPlayer) StaffPlayersManagers.getStaffPlayer(player);
 
         if(staffPlayer.isStaffMode()){
             StaffUtils.asyncStaffChatMessage(Messages.GET_STAFF_VANISH_LEAVE_MESSAGE()
                     .replace("{player}", player.getName()));
             event.setQuitMessage("");
+        }
+
+        if (Config.disableStaffModeOnExit()) {
+            staffPlayer.setQuit(true);
+            staffPlayer.toggleStaffMode(true);
         }
 
         if(staffPlayer.existsData() && staffPlayer.isStaffMode()){
