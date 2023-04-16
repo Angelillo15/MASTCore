e5cb5e409ad1f4fc1ad398bb610694889f05a62e
Angelillo15 <contact@angelillo15.es>
2023-04-16 21:10:06 +0200

    Addedd Database fields

--- MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/config/Config.java
+++ MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/config/Config.java
@@ -42,4 +42,30 @@ public class Config {
             return ConfigLoader.getConfig().getConfig().getString("Redis.serverID");
         }
     }
+
+    public static class Database {
+        public static String type() {
+            return ConfigLoader.getConfig().getConfig().getString("Database.type");
+        }
+
+        public static String host() {
+            return ConfigLoader.getConfig().getConfig().getString("Database.host");
+        }
+
+        public static int port() {
+            return ConfigLoader.getConfig().getConfig().getInt("Database.port");
+        }
+
+        public static String database() {
+            return ConfigLoader.getConfig().getConfig().getString("Database.database");
+        }
+
+        public static String username() {
+            return ConfigLoader.getConfig().getConfig().getString("Database.user");
+        }
+
+        public static String password() {
+            return ConfigLoader.getConfig().getConfig().getString("Database.password");
+        }
+    }
 }
