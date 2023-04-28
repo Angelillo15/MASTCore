b05148bca499527e3c306709bb9c973f003e8e3e
Angelillo15 <contact@angelillo15.es>
2023-04-29 00:49:45 +0200

    new logger that allows replaces

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/ILogger.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/ILogger.java
@@ -2,7 +2,35 @@ package es.angelillo15.mast.api;
 
 public interface ILogger {
     void info(String message);
+
     void warn(String message);
+
     void error(String message);
+
     void debug(String message);
+
+    default void info(String message, Object... args) {
+        info(format(message, args));
+    }
+
+    default void warn(String message, Object... args) {
+        warn(format(message, args));
+    }
+
+    default void error(String message, Object... args) {
+        error(format(message, args));
+    }
+
+    default void debug(String message, Object... args) {
+        debug(format(message, args));
+    }
+
+    default String format(String message, Object... args) {
+        String msg = message;
+        for (Object arg : args) {
+            msg = msg.replaceFirst("\\{\\}", arg.toString());
+        }
+
+        return msg;
+    }
 }
