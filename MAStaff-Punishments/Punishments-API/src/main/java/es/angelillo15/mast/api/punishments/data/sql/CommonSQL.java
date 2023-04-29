e0f6b4e60e95a82ecacea367347020595d389713
Angelillo15 <contact@angelillo15.es>
2023-04-29 14:27:45 +0200

    Change querie

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/data/sql/CommonSQL.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/data/sql/CommonSQL.java
@@ -20,18 +20,17 @@ public class CommonSQL extends AbstractDataManager {
         try (PreparedStatement statement =
                      PluginConnection.getConnection().prepareStatement(
                              "INSERT INTO `mastaff_punishments_bans` " +
-                                     "(`UUID`, `IP`, `reason`, `banned_by_uuid`, `banned_by_name`, `active`, `time`, `until`, `ipban`) " +
-                                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);")
+                                     "(`UUID`, `reason`, `banned_by_uuid`, `banned_by_name`, `active`, `time`, `until`, `ipban`) " +
+                                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?);")
         ){
             statement.setString(1, uuid);
-            statement.setString(2, ip);
-            statement.setString(3, reason);
-            statement.setString(4, banned_by_uuid);
-            statement.setString(5, banned_by_name);
-            statement.setBoolean(6, active);
-            statement.setLong(7, time);
-            statement.setLong(8, until);
-            statement.setBoolean(9, ipban);
+            statement.setString(2, reason);
+            statement.setString(3, banned_by_uuid);
+            statement.setString(4, banned_by_name);
+            statement.setBoolean(5, active);
+            statement.setLong(6, time);
+            statement.setLong(7, until);
+            statement.setBoolean(8, ipban);
 
             statement.execute();
         } catch (SQLException e) {
