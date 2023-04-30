a5226abb7c5779a30fc1ae5097275c34670594fb
Angelillo15 <contact@angelillo15.es>
2023-04-30 14:49:56 +0200

    Added a common event manager for spigot and bungeecord

---
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/events/BungeeEventManager.java
@@ -0,0 +1,19 @@
+package es.angelillo15.mast.api.punishments.events;
+
+import es.angelillo15.mast.api.cmd.sender.CommandSender;
+import es.angelillo15.mast.api.event.bungee.ban.PlayerBannedEvent;
+import es.angelillo15.mast.api.models.BanModel;
+import net.md_5.bungee.api.ProxyServer;
+
+public class BungeeEventManager extends EventManager {
+    @Override
+    public void sendPlayerBannedEvent(BanModel banModel, CommandSender sender) {
+        PlayerBannedEvent event = new PlayerBannedEvent(banModel, sender, (result, error) -> {
+            if (error != null) {
+                error.printStackTrace();
+            }
+        });
+
+        ProxyServer.getInstance().getPluginManager().callEvent(event);
+    }
+}
---
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/events/EventManager.java
@@ -0,0 +1,22 @@
+package es.angelillo15.mast.api.punishments.events;
+
+import es.angelillo15.mast.api.cmd.sender.CommandSender;
+import es.angelillo15.mast.api.models.BanModel;
+import es.angelillo15.mast.api.utils.ServerUtils;
+
+public abstract class EventManager {
+    private static EventManager eventManager;
+    public abstract void sendPlayerBannedEvent(BanModel banModel, CommandSender sender);
+
+    public static EventManager getEventManager() {
+        if (eventManager != null) {
+            return eventManager;
+        }
+
+        if (ServerUtils.getServerType() == ServerUtils.ServerType.BUNGEE) {
+            eventManager = new BungeeEventManager();
+        }
+
+        return eventManager;
+    }
+}
