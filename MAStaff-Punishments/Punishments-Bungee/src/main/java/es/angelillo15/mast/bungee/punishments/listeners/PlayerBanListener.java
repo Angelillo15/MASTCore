d624fb07453a707419863c4f8f90f08799f472a0
Angelillo15 <contact@angelillo15.es>
2023-04-30 14:51:38 +0200

    Added listener to PlayerBannedEvent

--- MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/listeners/PlayerBanListener.java
+++ MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/listeners/PlayerBanListener.java
@@ -4,26 +4,43 @@ import es.angelillo15.mast.api.MAStaffInstance;
 import es.angelillo15.mast.api.TextUtils;
 import es.angelillo15.mast.api.data.DataManager;
 import es.angelillo15.mast.api.data.UserData;
+import es.angelillo15.mast.api.event.bungee.ban.PlayerBannedEvent;
+import es.angelillo15.mast.api.models.BanModel;
 import es.angelillo15.mast.api.punishments.cache.BanCache;
 import es.angelillo15.mast.api.punishments.config.Config;
 import es.angelillo15.mast.api.punishments.config.Messages;
 import es.angelillo15.mast.api.punishments.data.AbstractDataManager;
-import es.angelillo15.mast.api.punishments.models.BanModel;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.PendingConnection;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
+
 import java.util.UUID;
 
-public class PlayerJoinListener implements Listener {
+public class PlayerBanListener implements Listener {
     @EventHandler
     public void onJoin(PreLoginEvent event) {
         PendingConnection conn = event.getConnection();
-
+        event.registerIntent((Plugin) MAStaffInstance.getBungeeInstance());
 
         if (cacheCheck(conn)) return;
         databaseCheck(conn);
+
+        event.completeIntent((Plugin) MAStaffInstance.getBungeeInstance());
+    }
+
+    @EventHandler
+    public void onPlayerBan(PlayerBannedEvent event) {
+        ProxiedPlayer banned = ProxyServer.getInstance().getPlayer(event.getBanModel().getUuid());
+
+        if (banned != null) {
+            banned.disconnect(getBaseComponent(event.getBanModel()));
+        }
     }
 
     private boolean cacheCheck(PendingConnection conn) {
@@ -69,24 +86,25 @@ public class PlayerJoinListener implements Listener {
     }
 
     private void disconnect(BanModel model, PendingConnection player) {
+        player.disconnect(getBaseComponent(model));
+    }
+
+    private BaseComponent getBaseComponent(BanModel model) {
         if (model.isPermanent()) {
-            player.disconnect(new TextComponent(
-                    Messages.Ban.bannedMessagePermanent()
-                            .replace("{reason}", model.getReason())
-                            .replace("{bannedBy}", model.getBannedBy())
-                            .replace("{bannedOn}", TextUtils.formatDate(model.getTime(), Config.dateFormat())
-                            )
-            ));
+            return new TextComponent(Messages.Ban.bannedMessagePermanent()
+                    .replace("{reason}", model.getReason())
+                    .replace("{bannedBy}", model.getBannedBy())
+                    .replace("{bannedOn}", TextUtils.formatDate(model.getTime(), Config.dateFormat()))
+            );
         } else {
-            player.disconnect(new TextComponent(
+            return new TextComponent(
                     Messages.Ban.tempBanMessageBase()
                             .replace("{reason}", model.getReason())
                             .replace("{bannedBy}", model.getBannedBy())
                             .replace("{bannedOn}", TextUtils.formatDate(model.getTime(), Config.dateFormat()))
                             .replace("{expires}", TextUtils.formatDate(model.getUntil(), Config.dateFormat()))
                             .replace("{duration}", TextUtils.formatUptime(model.getUntil()))
-            ));
+            );
         }
-
     }
 }
