1b6707df1dd7368b2235b1cafea50de1f7de9a26
Angelillo15 <contact@angelillo15.es>
2023-04-30 14:50:33 +0200

    Added Bungeecord api

--- MAStaff-Punishments/Punishments-API/build.gradle.kts
+++ MAStaff-Punishments/Punishments-API/build.gradle.kts
@@ -4,10 +4,12 @@ plugins {
 
 repositories {
     mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
     compileOnly("com.github.Angelillo15:ConfigManager:1.4")
     compileOnly("org.yaml:snakeyaml:1.33")
     compileOnly("com.github.Carleslc.Simple-YAML:Simple-Yaml:1.8.3")
+    compileOnly("io.github.waterfallmc:waterfall-api:1.19-R0.1-SNAPSHOT")
 }
