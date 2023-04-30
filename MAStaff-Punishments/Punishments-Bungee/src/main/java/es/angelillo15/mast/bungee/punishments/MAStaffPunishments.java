755edc776a26866812240ce258e29f76eeec4ec9
Angelillo15 <contact@angelillo15.es>
2023-04-30 14:06:41 +0200

    Register ClearCacheCMD

--- MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/MAStaffPunishments.java
+++ MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/MAStaffPunishments.java
@@ -2,14 +2,14 @@ package es.angelillo15.mast.bungee.punishments;
 
 import es.angelillo15.mast.api.MAStaffInstance;
 import es.angelillo15.mast.api.addons.MAStaffAddon;
-import es.angelillo15.mast.api.punishments.cmd.BanCMD;
-import es.angelillo15.mast.api.punishments.cmd.IsBanned;
+import es.angelillo15.mast.api.punishments.cmd.ban.BanCMD;
+import es.angelillo15.mast.api.punishments.cmd.ban.IsBannedCMD;
+import es.angelillo15.mast.api.punishments.cmd.cache.ClearCacheCMD;
 import es.angelillo15.mast.api.punishments.config.ConfigLoader;
 import es.angelillo15.mast.api.punishments.data.DataManager;
 import es.angelillo15.mast.api.punishments.cmd.TestCMD;
 import es.angelillo15.mast.bungee.punishments.listeners.PlayerJoinListener;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.plugin.Event;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 
@@ -34,7 +34,8 @@ public class MAStaffPunishments extends MAStaffAddon<Plugin> {
     public void loadCommands() {
         MAStaffInstance.getBungeeInstance().registerCommand(new TestCMD());
         MAStaffInstance.getBungeeInstance().registerCommand(new BanCMD());
-        MAStaffInstance.getBungeeInstance().registerCommand(new IsBanned());
+        MAStaffInstance.getBungeeInstance().registerCommand(new IsBannedCMD());
+        MAStaffInstance.getBungeeInstance().registerCommand(new ClearCacheCMD());
     }
 
     public void loadConfig() {
