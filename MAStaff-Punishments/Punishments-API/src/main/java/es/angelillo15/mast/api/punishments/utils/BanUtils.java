ba12dec515e16cd47fcf4e2b4a4e2fa969699659
Angelillo15 <contact@angelillo15.es>
2023-05-01 12:43:23 +0200

    Uses new username

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/utils/BanUtils.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/utils/BanUtils.java
@@ -5,26 +5,28 @@ import es.angelillo15.mast.api.data.UserData;
 import es.angelillo15.mast.api.punishments.data.DataManager;
 import es.angelillo15.mast.api.punishments.enums.ErrorTypes;
 
+import java.util.UUID;
+
 public class BanUtils {
-    public static void ban(String uuid, String ip, String reason, String banned_by_uuid, String banned_by_name, boolean active, long time, long until, boolean ipban) {
-        DataManager.getDataManager().ban(uuid, ip, reason, banned_by_uuid, banned_by_name, active, time, until, ipban);
+    public static void ban(String uuid, String username, String ip, String reason, String banned_by_uuid, String banned_by_name, boolean active, long time, long until, boolean ipban) {
+        DataManager.getDataManager().ban(uuid, username, ip, reason, banned_by_uuid, banned_by_name, active, time, until, ipban);
     }
 
     public static ErrorTypes ban(CommandSender banned_by, UserData data, String reason, long until, boolean ipban) {
         if (data == null) {
             return ErrorTypes.NULL_DATA;
         }
-        if (DataManager.getDataManager().isPermBanned(data.getUUID())) {
+        if (DataManager.getDataManager().isPermBanned(data.getUsername())) {
             return ErrorTypes.PLAYER_ALREADY_PERM_BANNED;
         }
 
-        ban(data.getUUID(), data.getLastIP(), reason, banned_by.getUniqueId(), banned_by.getName(), true, System.currentTimeMillis(), until, ipban);
+        ban(data.getUUID(), data.getUsername(), data.getLastIP(), reason, banned_by.getUniqueId(), banned_by.getName(), true, System.currentTimeMillis(), until, ipban);
 
         return ErrorTypes.SUCCESS;
     }
 
     public static boolean isBanned(String uuid) {
-        return DataManager.getDataManager().isTempBanned(uuid);
+        return DataManager.getDataManager().isTempBanned(UUID.fromString(uuid));
     }
 
     public static ErrorTypes permBan(CommandSender banned_by, UserData target, String reason, boolean ipban) {
