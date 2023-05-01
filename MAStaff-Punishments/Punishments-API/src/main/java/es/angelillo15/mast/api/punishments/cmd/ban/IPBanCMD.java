18b49beee3f99935b83d036e593e27ab6f90ab3f
Angelillo15 <contact@angelillo15.es>
2023-05-01 19:05:01 +0200

    Created IPBanCMD and TempIPBanCMd

---
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/IPBanCMD.java
@@ -0,0 +1,72 @@
+package es.angelillo15.mast.api.punishments.cmd.ban;
+
+import es.angelillo15.mast.api.cmd.Command;
+import es.angelillo15.mast.api.cmd.CommandData;
+import es.angelillo15.mast.api.cmd.sender.CommandSender;
+import es.angelillo15.mast.api.data.UserData;
+import es.angelillo15.mast.api.punishments.config.Messages;
+import es.angelillo15.mast.api.punishments.data.DataManager;
+import es.angelillo15.mast.api.punishments.enums.ErrorTypes;
+import es.angelillo15.mast.api.punishments.events.EventManager;
+import es.angelillo15.mast.api.punishments.utils.BanUtils;
+
+import java.util.UUID;
+
+@CommandData(
+        name = "ipban",
+        permission = "mastaff.punishments.ipban",
+        usage = "/ipban <ip> <reason>",
+        description = "Ban an IP"
+)
+public class IPBanCMD extends Command {
+    @Override
+    public void onCommand(CommandSender sender, String label, String[] args) {
+        new Thread(() -> {
+            if (args.length < 1) {
+                sender.sendMessage(Messages.Commands.IpBan.usage());
+                return;
+            }
+
+            String target = args[0];
+
+            StringBuilder reason = new StringBuilder();
+
+            for (int i = 1; i < args.length; i++) {
+                reason.append(args[i]).append(" ");
+            }
+
+            if (reason.toString().isEmpty()) {
+                reason.append(Messages.Default.defaultBanReason());
+            }
+
+            if (target == null) {
+                sender.sendMessage(Messages.Commands.playerNotFound(target));
+                return;
+            }
+            UserData data = BanUtils.getUserData(target);
+            ErrorTypes result = BanUtils.ban(sender, data , reason.toString(), 0, true);
+
+            if (result == ErrorTypes.NULL_DATA) {
+                sender.sendMessage(Messages.Commands.playerNotFound(target));
+                return;
+            }
+
+            if (result == ErrorTypes.PLAYER_ALREADY_PERM_BANNED) {
+                sender.sendMessage(Messages.Commands.playerAlreadyBanned(target));
+                return;
+            }
+
+            if (result == ErrorTypes.SUCCESS) {
+                sender.sendMessage(Messages.Commands.IpBan.success(target, reason.toString(), sender.getName()));
+                try {
+                    EventManager.getEventManager().sendPlayerBannedEvent(DataManager.getDataManager().getBan(UUID.fromString(data.getUUID())), sender);
+                } catch (Exception e) {
+                    sender.sendMessage(Messages.Commands.playerNotFound(target));
+                }
+                return;
+            }
+
+            sender.sendMessage("An error ocurred");
+        }).start();
+    }
+}
---
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/TempIPBanCMD.java
@@ -0,0 +1,75 @@
+package es.angelillo15.mast.api.punishments.cmd.ban;
+
+import es.angelillo15.mast.api.cmd.Command;
+import es.angelillo15.mast.api.cmd.CommandData;
+import es.angelillo15.mast.api.cmd.sender.CommandSender;
+import es.angelillo15.mast.api.data.UserData;
+import es.angelillo15.mast.api.punishments.config.Messages;
+import es.angelillo15.mast.api.punishments.data.DataManager;
+import es.angelillo15.mast.api.punishments.enums.ErrorTypes;
+import es.angelillo15.mast.api.punishments.events.EventManager;
+import es.angelillo15.mast.api.punishments.utils.BanUtils;
+import es.angelillo15.mast.api.utils.NumberUtils;
+
+import java.util.UUID;
+
+@CommandData(
+        name = "tempipban",
+        permission = "mast.ban.tempipban",
+        aliases = {"tempipban", "tempipb"},
+        usage = "&cUsage: &7/tempipban <player> <time> <reason>",
+        description = "Temporarily IP ban a player"
+)
+public class TempIPBanCMD extends Command {
+    @Override
+    public void onCommand(CommandSender sender, String label, String[] args) {
+        if (args.length < 2) {
+            sender.sendMessage(Messages.Commands.TempBan.usage());
+            return;
+        }
+
+        String target = args[0];
+
+        StringBuilder reason = new StringBuilder();
+
+        for (int i = 2; i < args.length; i++) {
+            reason.append(args[i]).append(" ");
+        }
+
+        if (reason.toString().isEmpty()) {
+            reason.append(Messages.Default.defaultBanReason());
+        }
+
+        long time;
+
+        try {
+            time = System.currentTimeMillis() + NumberUtils.parseToMilis(args[1]);
+        } catch (NumberFormatException e) {
+            sender.sendMessage("Invalid time");
+            return;
+        }
+
+        UserData data = BanUtils.getUserData(target);
+
+        ErrorTypes error = BanUtils.ban(sender, data, reason.toString(), time, true);
+
+        if (error == ErrorTypes.NULL_DATA) {
+            sender.sendMessage(Messages.Commands.playerNotFound(target));
+            return;
+        }
+
+        if (error == ErrorTypes.PLAYER_ALREADY_PERM_BANNED) {
+            sender.sendMessage(Messages.Commands.playerAlreadyBanned(target));
+            return;
+        }
+
+        if (error == ErrorTypes.SUCCESS) {
+            try {
+                EventManager.getEventManager().sendPlayerBannedEvent(DataManager.getDataManager().getBan(UUID.fromString(data.getUUID())), sender);
+            } catch (Exception e) {
+                sender.sendMessage(Messages.Commands.playerNotFound(target));
+            }
+            sender.sendMessage(Messages.Commands.TempBan.success(target, args[1], reason.toString(), sender.getName()));
+        }
+    }
+}
