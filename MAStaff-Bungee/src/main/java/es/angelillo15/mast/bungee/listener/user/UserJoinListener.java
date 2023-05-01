8c0b87c278ff2c1b60e23945eb41b8ba94399551
Angelillo15 <contact@angelillo15.es>
2023-05-01 12:45:25 +0200

    Added event priority

--- MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/listener/user/UserJoinListener.java
+++ MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/listener/user/UserJoinListener.java
@@ -8,9 +8,12 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
 
 public class UserJoinListener implements Listener {
-    @EventHandler
+    @EventHandler(
+            priority = EventPriority.LOWEST
+    )
     public void onUserJoin(PostLoginEvent event) {
         ProxiedPlayer player = event.getPlayer();
 
