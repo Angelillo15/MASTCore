e33b70a4abcc2878986cfda37d47e4eea503d1bc
Angelillo15 <contact@angelillo15.es>
2023-05-01 12:43:51 +0200

    Added new event

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/events/BungeeEventManager.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/events/BungeeEventManager.java
@@ -1,6 +1,7 @@
 package es.angelillo15.mast.api.punishments.events;
 
 import es.angelillo15.mast.api.cmd.sender.CommandSender;
+import es.angelillo15.mast.api.event.bungee.ban.BannedPlayerTriesToJoin;
 import es.angelillo15.mast.api.event.bungee.ban.PlayerBannedEvent;
 import es.angelillo15.mast.api.models.BanModel;
 import net.md_5.bungee.api.ProxyServer;
@@ -13,6 +14,13 @@ public class BungeeEventManager extends EventManager {
                 error.printStackTrace();
             }
         });
+
+        ProxyServer.getInstance().getPluginManager().callEvent(event);
+    }
+
+    @Override
+    public void sendPlayerTryToJoinBannedEvent(BanModel banModel, String player) {
+        BannedPlayerTriesToJoin event = new BannedPlayerTriesToJoin(banModel, player);
 
         ProxyServer.getInstance().getPluginManager().callEvent(event);
     }
