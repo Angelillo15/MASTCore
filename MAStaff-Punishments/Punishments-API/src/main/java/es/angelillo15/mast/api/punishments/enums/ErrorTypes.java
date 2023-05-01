4d1472059402734484a249eb521b50c4d44537dd
Angelillo15 <contact@angelillo15.es>
2023-05-01 19:06:00 +0200

    Deleted unused error types

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/enums/ErrorTypes.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/enums/ErrorTypes.java
@@ -1,10 +1,7 @@
 package es.angelillo15.mast.api.punishments.enums;
 
 public enum ErrorTypes {
-    PLAYER_NOT_FOUND,
     PLAYER_ALREADY_PERM_BANNED,
-    PLAYER_ALREADY_TEMP_BANNED,
-    PLAYER_ALREADY_PERM_MUTED,
     NULL_DATA,
     SUCCESS
 }
