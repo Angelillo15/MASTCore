7aa3137201b55b43d0cba491db226c932083cdf0
Angelillo15 <contact@angelillo15.es>
2023-05-02 12:56:55 +0200

    Debug message when player added

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/PunishPlayersManager.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/PunishPlayersManager.java
@@ -1,5 +1,7 @@
 package es.angelillo15.mast.api.punishments;
 
+import es.angelillo15.mast.api.MAStaffInstance;
+
 import java.util.HashMap;
 
 public class PunishPlayersManager {
@@ -13,7 +15,8 @@ public class PunishPlayersManager {
     }
 
     public static void addPlayer(IPunishPlayer player) {
-        players.put(player.getUUID().toString(), player);
+        players.put(player.getUUID(), player);
+        MAStaffInstance.getLogger().debug("Added player " + player.getName() + " (" + player.getUUID() + ") to the cache");
     }
 
     public static void removePlayer(String uuid) {
@@ -26,5 +29,9 @@ public class PunishPlayersManager {
 
     public static void clearCache() {
         players.clear();
+    }
+
+    public static HashMap<String, IPunishPlayer> getPlayers() {
+        return players;
     }
 }
