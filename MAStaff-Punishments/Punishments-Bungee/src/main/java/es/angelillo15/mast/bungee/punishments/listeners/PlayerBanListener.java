545ad912974e9afa4443278ed77e3fe290746f16
Angelillo15 <contact@angelillo15.es>
2023-05-02 12:58:01 +0200

    Change checks on pre login

--- MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/listeners/PlayerBanListener.java
+++ MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/listeners/PlayerBanListener.java
@@ -1,18 +1,22 @@
 package es.angelillo15.mast.bungee.punishments.listeners;
 
 import com.craftmend.storm.Storm;
+import es.angelillo15.mast.api.MAStaffInstance;
 import es.angelillo15.mast.api.TextUtils;
 import es.angelillo15.mast.api.database.PluginConnection;
-import es.angelillo15.mast.api.punishments.config.Config;
-import es.angelillo15.mast.api.punishments.config.Messages;
-import es.angelillo15.mast.api.punishments.data.migrations.BansTable;
-import es.angelillo15.mast.api.punishments.data.migrations.IpBansTable;
+import es.angelillo15.mast.api.config.punishments.Config;
+import es.angelillo15.mast.api.config.punishments.Messages;
+import es.angelillo15.mast.api.models.BansTable;
+import es.angelillo15.mast.api.models.IpBansTable;
+import es.angelillo15.mast.api.punishments.events.EventManager;
+import lombok.SneakyThrows;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
 import java.sql.SQLException;
@@ -20,15 +24,19 @@ import java.sql.SQLException;
 public class PlayerBanListener implements Listener {
     @EventHandler
     public void onPreLogin(PreLoginEvent event) {
-         PendingConnection connection = event.getConnection();
+        event.registerIntent((Plugin) MAStaffInstance.getBungeeInstance());
+        PendingConnection connection = event.getConnection();
 
-         if (banCheck(connection)) {
-             event.setCancelled(true);
-             return;
-         }
+        if (banCheck(connection)) {
+            event.completeIntent((Plugin) MAStaffInstance.getBungeeInstance());
+            return;
+        }
 
-         ipBanCheck(connection);
+        ipBanCheck(connection);
+
+        event.completeIntent((Plugin) MAStaffInstance.getBungeeInstance());
     }
+
     @EventHandler
     public void onPostLogin(PostLoginEvent event) {
         Storm storm = PluginConnection.getStorm();
@@ -70,7 +78,8 @@ public class PlayerBanListener implements Listener {
         try {
             BansTable ban = BansTable.getBan(connection.getName());
             connection.disconnect(getBaseComponent(ban));
-        } catch (Exception ignored) {}
+        } catch (Exception ignored) {
+        }
 
         return true;
     }
@@ -84,8 +93,11 @@ public class PlayerBanListener implements Listener {
 
         return true;
     }
+
     private BaseComponent getBaseComponent(BansTable model) {
         String message = null;
+        EventManager.getEventManager().sendPlayerTryToJoinBannedEvent(model, model.getUsername());
+
         if (model.isPermanent()) {
             if (model.getIpban()) message = Messages.Ban.ipBannedMessagePermanent();
             else message = Messages.Ban.bannedMessagePermanent();
