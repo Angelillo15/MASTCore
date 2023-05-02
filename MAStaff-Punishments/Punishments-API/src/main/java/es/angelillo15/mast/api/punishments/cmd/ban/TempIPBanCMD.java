e504dc3d8b513ed7c34b177bd579c3b39c9b0aa9
Angelillo15 <contact@angelillo15.es>
2023-05-02 12:57:20 +0200

    Fill tempbanip command using new API

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/TempIPBanCMD.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/TempIPBanCMD.java
@@ -1,10 +1,14 @@
 package es.angelillo15.mast.api.punishments.cmd.ban;
 
+import es.angelillo15.mast.api.TextUtils;
 import es.angelillo15.mast.api.cmd.Command;
 import es.angelillo15.mast.api.cmd.CommandData;
 import es.angelillo15.mast.api.cmd.sender.CommandSender;
-
-import java.util.UUID;
+import es.angelillo15.mast.api.punishments.IPunishPlayer;
+import es.angelillo15.mast.api.punishments.PunishPlayersManager;
+import es.angelillo15.mast.api.config.punishments.Config;
+import es.angelillo15.mast.api.config.punishments.Messages;
+import es.angelillo15.mast.api.utils.NumberUtils;
 
 @CommandData(
         name = "tempipban",
@@ -16,6 +20,41 @@ import java.util.UUID;
 public class TempIPBanCMD extends Command {
     @Override
     public void onCommand(CommandSender sender, String label, String[] args) {
+        IPunishPlayer punishPlayer = PunishPlayersManager.getPlayer(sender.getUniqueId());
 
+        if (args.length < 2) {
+            sender.sendMessage(Messages.Commands.TempIpBan.usage());
+            return;
+        }
+
+        String target = args[0];
+
+        StringBuilder reason = new StringBuilder();
+
+        for (int i = 2; i < args.length; i++) {
+            reason.append(args[i]).append(" ");
+        }
+
+        if (reason.toString().isEmpty()) {
+            reason.append(Messages.Default.defaultBanReason());
+        }
+
+        long time;
+
+        try {
+            time = System.currentTimeMillis() + NumberUtils.parseToMilis(args[1]);
+        } catch (NumberFormatException e) {
+            sender.sendMessage("Invalid time");
+            return;
+        }
+        new Thread(() -> {
+            punishPlayer.ban(target, reason.toString(), time, true);
+            sender.sendMessage(Messages.Commands.TempIpBan.success(
+                    target,
+                    TextUtils.formatDate(time, Config.dateFormat()),
+                    reason.toString(),
+                    sender.getName())
+            );
+        }).start();
     }
 }
