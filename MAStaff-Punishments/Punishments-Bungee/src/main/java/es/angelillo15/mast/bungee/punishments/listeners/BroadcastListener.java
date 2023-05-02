2bafd08cb574b02c06a439e8d42e18fa086fbd9d
Angelillo15 <contact@angelillo15.es>
2023-05-02 13:43:37 +0200

    Added banned listener to kick on ban

--- MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/listeners/BroadcastListener.java
+++ MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/listeners/BroadcastListener.java
@@ -7,6 +7,7 @@ import es.angelillo15.mast.api.event.bungee.ban.PlayerBannedEvent;
 import es.angelillo15.mast.api.config.punishments.Config;
 import es.angelillo15.mast.api.config.punishments.Messages;
 import es.angelillo15.mast.api.models.BansTable;
+import es.angelillo15.mast.api.models.IpBansTable;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -57,6 +58,23 @@ public class BroadcastListener implements Listener {
                     TextUtils.formatUptime(ban.getUntil() - System.currentTimeMillis()),
                     TextUtils.formatDate(ban.getUntil(), Config.dateFormat())
             ), "mast.punishments.ban.notify");
+        }
+
+        MAStaffInstance.getLogger().debug("PlayerBannedEvent triggered");
+        ProxiedPlayer player = ProxyServer.getInstance().getPlayer(event.getBanModel().getUsername());
+
+        if (player != null) {
+            player.disconnect(PlayerBanListener.getBaseComponent(event.getBanModel(), player.getName()));
+        }
+        MAStaffInstance.getLogger().debug("event.getBanModel().getIpban() = " + event.getBanModel().getIpban());
+        if (event.getBanModel().getIpban()) {
+            IpBansTable ipban = IpBansTable.getIpBanned(event.getBanModel().getId());
+            MAStaffInstance.getLogger().debug("IpBansTable.getIpBanned(" + event.getBanModel().getId() + ") returned " + ipban);
+            for (ProxiedPlayer p : ProxyServer.getInstance().getPlayers()) {
+                if (p.getAddress().getAddress().getHostAddress().equals(ipban.getIp())) {
+                    p.disconnect(PlayerBanListener.getBaseComponent(event.getBanModel(), p.getName()));
+                }
+            }
         }
     }
 
