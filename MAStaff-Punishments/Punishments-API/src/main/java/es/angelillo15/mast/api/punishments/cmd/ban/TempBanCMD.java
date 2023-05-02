62f03cf173dcaf92efa5f29e76c446a27b098075
Angelillo15 <contact@angelillo15.es>
2023-05-02 12:57:16 +0200

    Fill tempban command using new API

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/TempBanCMD.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/TempBanCMD.java
@@ -1,8 +1,14 @@
 package es.angelillo15.mast.api.punishments.cmd.ban;
 
+import es.angelillo15.mast.api.TextUtils;
 import es.angelillo15.mast.api.cmd.Command;
 import es.angelillo15.mast.api.cmd.CommandData;
 import es.angelillo15.mast.api.cmd.sender.CommandSender;
+import es.angelillo15.mast.api.punishments.IPunishPlayer;
+import es.angelillo15.mast.api.punishments.PunishPlayersManager;
+import es.angelillo15.mast.api.config.punishments.Config;
+import es.angelillo15.mast.api.config.punishments.Messages;
+import es.angelillo15.mast.api.utils.NumberUtils;
 
 @CommandData(
         name = "tempban",
@@ -12,6 +18,42 @@ import es.angelillo15.mast.api.cmd.sender.CommandSender;
 public class TempBanCMD extends Command {
     @Override
     public void onCommand(CommandSender sender, String label, String[] args) {
+        IPunishPlayer punishPlayer = PunishPlayersManager.getPlayer(sender.getUniqueId());
 
+        if (args.length < 2) {
+            sender.sendMessage(Messages.Commands.TempBan.usage());
+            return;
+        }
+
+        String target = args[0];
+
+        StringBuilder reason = new StringBuilder();
+
+        for (int i = 2; i < args.length; i++) {
+            reason.append(args[i]).append(" ");
+        }
+
+        if (reason.toString().isEmpty()) {
+            reason.append(Messages.Default.defaultBanReason());
+        }
+
+        long time;
+
+        try {
+            time = System.currentTimeMillis() + NumberUtils.parseToMilis(args[1]);
+        } catch (NumberFormatException e) {
+            sender.sendMessage("Invalid time");
+            return;
+        }
+
+        new Thread(() -> {
+            punishPlayer.ban(target, reason.toString(), time);
+            sender.sendMessage(Messages.Commands.TempBan.success(
+                    target,
+                    TextUtils.formatDate(time, Config.dateFormat()),
+                    reason.toString(),
+                    sender.getName())
+            );
+        }).start();
     }
 }
