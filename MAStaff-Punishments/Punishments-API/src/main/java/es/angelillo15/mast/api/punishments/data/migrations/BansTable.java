8f8d7fd808c3ba45d6580ed331a5bb8c52c2e5b7
Angelillo15 <contact@angelillo15.es>
2023-05-02 10:30:15 +0200

    Added ban models utils methods

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/data/migrations/BansTable.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/data/migrations/BansTable.java
@@ -1,10 +1,16 @@
 package es.angelillo15.mast.api.punishments.data.migrations;
 
+import com.craftmend.storm.Storm;
 import com.craftmend.storm.api.StormModel;
-import com.craftmend.storm.api.enums.KeyType;
+import com.craftmend.storm.api.enums.Where;
 import com.craftmend.storm.api.markers.Column;
 import com.craftmend.storm.api.markers.Table;
+import es.angelillo15.mast.api.database.PluginConnection;
+import es.angelillo15.mast.api.punishments.cache.BanCache;
 import lombok.Data;
+import lombok.SneakyThrows;
+
+import java.util.UUID;
 
 @Data
 @Table(name = "mastaff_bans")
@@ -41,4 +47,148 @@ public class BansTable extends StormModel {
 
     @Column
     private Boolean ipban;
+
+    public boolean isPermanent() {
+        return until == 0;
+    }
+    public boolean isExpired() {
+        return !isPermanent() && until < System.currentTimeMillis();
+    }
+
+    @SneakyThrows
+    public void unBan() {
+        Storm storm = PluginConnection.getStorm();
+
+        active = false;
+        storm.save(this);
+
+        if (!ipban) return;
+
+        IpBansTable ipBansTable = storm.buildQuery(IpBansTable.class)
+                .where("banId", Where.EQUAL, getId())
+                .limit(1)
+                .execute()
+                .join()
+                .iterator()
+                .next();
+
+        if (ipBansTable != null) {
+            storm.delete(ipBansTable);
+        }
+    }
+
+    @SneakyThrows
+    public static boolean isBanned(String username) {
+        Storm storm = PluginConnection.getStorm();
+
+        if (BanCache.isPunished(username)) return true;
+
+        boolean isBanned = storm.buildQuery(BansTable.class)
+                .where("username", Where.EQUAL, username)
+                .where("active", Where.EQUAL, true)
+                .limit(1)
+                .execute()
+                .join()
+                .size() > 0;
+
+        if (isBanned) {
+            new Thread(() -> {
+                BanCache.addPunishment(username, getBan(username));
+            }).start();
+        }
+
+        return isBanned;
+    }
+
+    @SneakyThrows
+    public static boolean isPermBanned(String username) {
+        Storm storm = PluginConnection.getStorm();
+
+        if (BanCache.isPunished(username)) return true;
+
+        boolean isBanned = storm.buildQuery(BansTable.class)
+                .where("username", Where.EQUAL, username)
+                .where("active", Where.EQUAL, true)
+                .where("until", Where.EQUAL, 0)
+                .limit(1)
+                .execute()
+                .join()
+                .size() > 0;
+
+        if (isBanned) {
+            new Thread(() -> {
+                BanCache.addPunishment(username, getBan(username));
+            }).start();
+        }
+
+        return isBanned;
+    }
+
+    public static BansTable getBan(String username) {
+        Storm storm = PluginConnection.getStorm();
+
+        try {
+            BansTable ban =  storm.buildQuery(BansTable.class)
+                    .where("username", Where.EQUAL, username)
+                    .where("active", Where.EQUAL, true)
+                    .limit(1)
+                    .execute()
+                    .join()
+                    .iterator()
+                    .next();
+
+            if (ban.isExpired()) {
+                ban.unBan();
+                return null;
+            }
+            return ban ;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @SneakyThrows
+    public static BansTable getBan(Integer id) {
+        Storm storm = PluginConnection.getStorm();
+
+        return storm.buildQuery(BansTable.class)
+                .where("id", Where.EQUAL, id)
+                .limit(1)
+                .execute()
+                .join()
+                .iterator()
+                .next();
+    }
+
+    public static BansTable getBan(UUID uuid) {
+        Storm storm = PluginConnection.getStorm();
+        try {
+            return storm.buildQuery(BansTable.class)
+                    .where("uuid", Where.EQUAL, uuid.toString())
+                    .where("active", Where.EQUAL, true)
+                    .limit(1)
+                    .execute()
+                    .join()
+                    .iterator()
+                    .next();
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    @SneakyThrows
+    public IpBansTable getIpBanTable(){
+        Storm storm = PluginConnection.getStorm();
+
+        if (!ipban) return null;
+
+        return storm.buildQuery(IpBansTable.class)
+                .where("banId", Where.EQUAL, getId())
+                .limit(1)
+                .execute()
+                .join()
+                .iterator()
+                .next();
+    }
 }
