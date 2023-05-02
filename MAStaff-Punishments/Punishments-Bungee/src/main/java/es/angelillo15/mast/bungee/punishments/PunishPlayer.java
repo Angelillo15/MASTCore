be621648b215aecd7f0c938513d38ec788e13f64
Angelillo15 <contact@angelillo15.es>
2023-05-02 12:56:43 +0200

    Add unban method

--- MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/PunishPlayer.java
+++ MAStaff-Punishments/Punishments-Bungee/src/main/java/es/angelillo15/mast/bungee/punishments/PunishPlayer.java
@@ -5,12 +5,14 @@ import es.angelillo15.mast.api.MAStaffInstance;
 import es.angelillo15.mast.api.cmd.sender.CommandSender;
 import es.angelillo15.mast.api.data.UserData;
 import es.angelillo15.mast.api.database.PluginConnection;
+import es.angelillo15.mast.api.exceptions.PlayerNotBannedException;
 import es.angelillo15.mast.api.managers.UserDataManager;
 import es.angelillo15.mast.api.punishments.IPunishPlayer;
-import es.angelillo15.mast.api.punishments.cache.BanCache;
-import es.angelillo15.mast.api.punishments.config.Messages;
-import es.angelillo15.mast.api.punishments.data.migrations.BansTable;
-import es.angelillo15.mast.api.punishments.data.migrations.IpBansTable;
+import es.angelillo15.mast.api.cache.BanCache;
+import es.angelillo15.mast.api.config.punishments.Messages;
+import es.angelillo15.mast.api.models.BansTable;
+import es.angelillo15.mast.api.models.IpBansTable;
+import es.angelillo15.mast.api.punishments.events.EventManager;
 import lombok.SneakyThrows;
 
 import java.util.Objects;
@@ -91,5 +93,34 @@ public class PunishPlayer implements IPunishPlayer {
         }
 
         BanCache.addPunishment(target, bansTable);
+        EventManager.getEventManager().sendPlayerBannedEvent(bansTable, player);
+    }
+
+    @SneakyThrows
+    @Override
+    public void unban(String target, String reason) {
+        Storm storm = PluginConnection.getStorm();
+
+        BansTable bansTable = null;
+
+        if (!BansTable.isPermBanned(target)) {
+            player.sendMessage(
+                    Messages.Commands.playerNotBanned(target)
+            );
+
+            throw new PlayerNotBannedException("Player " + target + " is not banned");
+        }
+
+        bansTable = BansTable.getBan(target);
+
+        bansTable.unBan(player.getName(), reason, player.getUniqueId());
+
+        MAStaffInstance.getLogger().debug("Unbanned " + target +
+                " by " + player.getName() +
+                " (" + player.getUniqueId() + ")" +
+                " with id " + bansTable.getId()
+        );
+
+        BanCache.removePunishment(target);
     }
 }
