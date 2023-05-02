ecace8662afd318d47eb1d37a44907f10eb89954
Angelillo15 <contact@angelillo15.es>
2023-05-02 12:57:26 +0200

    Fill unban command using new API

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/UnBanCMD.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/UnBanCMD.java
@@ -1,8 +1,12 @@
 package es.angelillo15.mast.api.punishments.cmd.ban;
 
+import es.angelillo15.mast.api.MAStaffInstance;
 import es.angelillo15.mast.api.cmd.Command;
 import es.angelillo15.mast.api.cmd.CommandData;
 import es.angelillo15.mast.api.cmd.sender.CommandSender;
+import es.angelillo15.mast.api.punishments.IPunishPlayer;
+import es.angelillo15.mast.api.punishments.PunishPlayersManager;
+import es.angelillo15.mast.api.config.punishments.Messages;
 
 @CommandData(
         name = "unban",
@@ -14,6 +18,35 @@ import es.angelillo15.mast.api.cmd.sender.CommandSender;
 public class UnBanCMD extends Command {
     @Override
     public void onCommand(CommandSender sender, String label, String[] args) {
+        IPunishPlayer punishPlayer = PunishPlayersManager.getPlayer(sender.getUniqueId());
 
+        if (args.length < 1) {
+            sender.sendMessage(Messages.Commands.Unban.usage());
+            return;
+        }
+
+        StringBuilder reason = new StringBuilder();
+
+        for (int i = 1; i < args.length; i++) {
+            reason.append(args[i]).append(" ");
+        }
+
+        if (reason.toString().isEmpty()) {
+            reason.append(Messages.Default.defaultUnbanReason());
+        }
+
+        String target = args[0];
+        new Thread(() -> {
+            try {
+                punishPlayer.unban(target, reason.toString());
+            } catch (Exception e) {
+                MAStaffInstance.getLogger().debug("Error while unbanning player " + target + ": " + e.getMessage());
+                return;
+            }
+            sender.sendMessage(Messages.Commands.Unban.success(
+                    target,
+                    reason.toString()
+            ));
+        }).start();
     }
 }
