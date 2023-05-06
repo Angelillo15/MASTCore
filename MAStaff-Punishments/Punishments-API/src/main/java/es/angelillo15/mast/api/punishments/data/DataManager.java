ce3306a1581d8b48fc66a7b89ead6c3d68052959
Angelillo15 <contact@angelillo15.es>
2023-05-06 21:28:16 +0200

    Register UserModel

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/data/DataManager.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/data/DataManager.java
@@ -4,6 +4,8 @@ import com.craftmend.storm.Storm;
 import es.angelillo15.mast.api.database.PluginConnection;
 import es.angelillo15.mast.api.models.BansTable;
 import es.angelillo15.mast.api.models.IpBansTable;
+import es.angelillo15.mast.api.models.ReportComments;
+import es.angelillo15.mast.api.models.ReportModel;
 import lombok.SneakyThrows;
 
 public class DataManager {
@@ -14,7 +16,7 @@ public class DataManager {
 
         storm.registerModel(new BansTable());
         storm.registerModel(new IpBansTable());
-
-        storm.runMigrations();
+        storm.registerModel(new ReportModel());
+        storm.registerModel(new ReportComments());
     }
 }
