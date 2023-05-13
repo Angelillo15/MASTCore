bb300eae632c2e7a76165c37e51db5a64470e745
Angelillo15 <contact@angelillo15.es>
2023-05-13 12:45:54 +0200

    Expire after time warn

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/config/punishments/Config.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/config/punishments/Config.java
@@ -11,4 +11,10 @@ public class Config {
     public static String dateFormat() {
         return ConfigLoader.getConfig().getConfig().getString("Config.date-format");
     }
+
+    public static class Warn {
+        public static String expireAfter() {
+            return ConfigLoader.getConfig().getConfig().getString("Warn.expire-after");
+        }
+    }
 }
