cd448d6e3772c766539fe865ed4a7560f9fb4f17
Angelillo15 <contact@angelillo15.es>
2023-06-18 15:35:22 +0200

    Added MiniMessage setup methods

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/TextUtils.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/TextUtils.java
@@ -1,8 +1,14 @@
 package es.angelillo15.mast.api;
 
 import es.angelillo15.mast.api.chat.api.ChatColor;
+import lombok.Getter;
+import lombok.Setter;
 import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.platform.AudienceProvider;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,6 +22,13 @@ public class TextUtils {
     private static final long MINUTE = 60 * SECOND;
     private static final long HOUR = 60 * MINUTE;
     private static final long DAY = 24 * HOUR;
+
+    @Getter
+    private static AudienceProvider audienceProvider;
+
+    public static void initAudienceProviderBukkit(JavaPlugin plugin) {
+        audienceProvider = BukkitAudiences.create(plugin);
+    }
 
     public static String colorize(String text) {
         if (MAStaffInstance.version() < 16) {
@@ -101,6 +114,41 @@ public class TextUtils {
             uptime = uptime % SECOND;
         }
         return buf.toString();
+    }
+
+    public static String toMM(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        Matcher m = ChatColor.STRIP_COLOR_PATTERN.matcher(sb);
+        while (m.find()) {
+            sb.replace(m.start(), m.end(), sb.substring(m.start(), m.end()).toLowerCase());
+        }
+        return sb.toString()
+                .replace("&0", "<black>")
+                .replace("&1", "<dark_blue>")
+                .replace("&2", "<dark_green>")
+                .replace("&3", "<dark_aqua>")
+                .replace("&4", "<dark_red>")
+                .replace("&5", "<dark_purple>")
+                .replace("&6", "<gold>")
+                .replace("&7", "<grey>")
+                .replace("&8", "<dark_grey>")
+                .replace("&9", "<blue>")
+                .replace("&a", "<green>")
+                .replace("&b", "<aqua>")
+                .replace("&c", "<red>")
+                .replace("&d", "<light_purple>")
+                .replace("&e", "<yellow>")
+                .replace("&f", "<white>")
+                .replace("&k", "<obf>")
+                .replace("&l", "<b>")
+                .replace("&m", "<st>")
+                .replace("&n", "<u>")
+                .replace("&o", "<i>")
+                .replace("&r", "<r>");
+    }
+
+    public static Component toComponent(String str) {
+        return Component.text(toMM(str));
     }
 
     public static String formatDate(long date, String format) {
