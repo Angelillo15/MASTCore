7a0c3ae528dba6c9ed64e55690649fbfc46f7254
Angelillo15 <contact@angelillo15.es>
2023-06-22 18:17:18 +0200

    Bug fix on Vanish Listener that makes the player invisible on Leave after Staff enable disable and rejoin

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/listener/VanishListener.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/listener/VanishListener.java
@@ -1,15 +1,17 @@
 package es.angelillo15.mast.bukkit.listener;
 
 import es.angelillo15.mast.api.Permissions;
+import es.angelillo15.mast.api.managers.StaffPlayersManagers;
 import es.angelillo15.mast.api.managers.VanishedPlayers;
 import es.angelillo15.mast.bukkit.MAStaff;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class VanishListener implements Listener {
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     @SuppressWarnings("deprecation")
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
@@ -21,6 +23,18 @@ public class VanishListener implements Listener {
         VanishedPlayers.getVanishedPlayers().forEach(vanishedPlayer -> {
             player.hidePlayer(vanishedPlayer);
         });
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    @SuppressWarnings("deprecation")
+    public void onLeave(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        if (StaffPlayersManagers.isStaffPlayer(player)) {
+            return;
+        }
+
+        VanishedPlayers.removePlayer(player);
     }
 
 }
