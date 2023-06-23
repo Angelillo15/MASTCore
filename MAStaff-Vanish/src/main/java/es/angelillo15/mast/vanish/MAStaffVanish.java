4ff92347906af3df0c547d4e2ed29e2fb032fcb0
Angelillo15 <contact@angelillo15.es>
2023-06-23 12:24:31 +0200

    Added Listener

--- MAStaff-Vanish/src/main/java/es/angelillo15/mast/vanish/MAStaffVanish.java
+++ MAStaff-Vanish/src/main/java/es/angelillo15/mast/vanish/MAStaffVanish.java
@@ -2,6 +2,7 @@ package es.angelillo15.mast.vanish;
 
 import es.angelillo15.mast.api.addons.MAStaffAddon;
 import es.angelillo15.mast.api.utils.ServerUtils;
+import es.angelillo15.mast.vanish.listeners.VanishListener;
 import es.angelillo15.mast.vanish.packets.BukkitPacketUtils;
 import es.angelillo15.mast.vanish.packets.IPacketUtils;
 import es.angelillo15.mast.vanish.packets.ProtocolLibPacketUtils;
@@ -18,6 +19,7 @@ public class MAStaffVanish extends MAStaffAddon<JavaPlugin> {
         getLogger().info("Enabling MAStaff Vanish module...");
         useProtocolLib = ServerUtils.isProtocolLibInstalled();
         loadPackets();
+        registerListeners();
     }
 
     private void loadPackets() {
@@ -30,13 +32,7 @@ public class MAStaffVanish extends MAStaffAddon<JavaPlugin> {
         }
     }
 
-    @Override
-    public void onDisable() {
-
-    }
-
-    @Override
-    public void reload() {
-
+    void registerListeners() {
+        getMastaffInstance().getServer().getPluginManager().registerEvents(new VanishListener(), getMastaffInstance());
     }
 }
