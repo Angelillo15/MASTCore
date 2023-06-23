bfb9215f4186ebd567dd627cc95095cb3b1e2af8
Angelillo15 <contact@angelillo15.es>
2023-06-23 12:23:21 +0200

    Added Packet Loader

--- MAStaff-Vanish/src/main/java/es/angelillo15/mast/vanish/MAStaffVanish.java
+++ MAStaff-Vanish/src/main/java/es/angelillo15/mast/vanish/MAStaffVanish.java
@@ -1,13 +1,33 @@
 package es.angelillo15.mast.vanish;
 
 import es.angelillo15.mast.api.addons.MAStaffAddon;
+import es.angelillo15.mast.api.utils.ServerUtils;
+import es.angelillo15.mast.vanish.packets.BukkitPacketUtils;
+import es.angelillo15.mast.vanish.packets.IPacketUtils;
+import es.angelillo15.mast.vanish.packets.ProtocolLibPacketUtils;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MAStaffVanish extends MAStaffAddon<JavaPlugin> {
+    @Getter
+    private static boolean useProtocolLib;
+    @Getter
+    private static IPacketUtils packetUtils;
     @Override
     public void onEnable() {
         getLogger().info("Enabling MAStaff Vanish module...");
-        loadConfig();
+        useProtocolLib = ServerUtils.isProtocolLibInstalled();
+        loadPackets();
+    }
+
+    private void loadPackets() {
+        if (useProtocolLib) {
+            packetUtils = new ProtocolLibPacketUtils();
+            getLogger().debug("Using ProtocolLib packet utils");
+        } else {
+            packetUtils = new BukkitPacketUtils();
+            getLogger().debug("Using Bukkit packet utils");
+        }
     }
 
     @Override
