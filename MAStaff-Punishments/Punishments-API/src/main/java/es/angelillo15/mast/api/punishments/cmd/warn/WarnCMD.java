6176e1dc63f512b65350782281d9907b7d79be55
Angelillo15 <contact@angelillo15.es>
2023-06-27 13:26:16 +0200

    Improvements in warn cmd

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/warn/WarnCMD.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/warn/WarnCMD.java
@@ -10,57 +10,37 @@ import es.angelillo15.mast.api.database.PluginConnection;
 import es.angelillo15.mast.api.managers.UserDataManager;
 import es.angelillo15.mast.api.models.UserModel;
 import es.angelillo15.mast.api.models.WarnModel;
+import es.angelillo15.mast.api.punishments.IPunishPlayer;
+import es.angelillo15.mast.api.punishments.PunishPlayersManager;
+import es.angelillo15.mast.api.punishments.cmd.PunishCommand;
 import es.angelillo15.mast.api.utils.NumberUtils;
 
 @CommandData(
         name = "warn",
         permission = "mast.warn"
 )
-public class WarnCMD extends Command {
+public class WarnCMD extends PunishCommand {
     @Override
-    public void onCommand(CommandSender sender, String label, String[] args) {
+    public void onCommand(IPunishPlayer sender, String label, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage(Messages.Commands.Warn.usage());
+            return;
+        }
+
+        String target = args[0];
+
+        StringBuilder reason = new StringBuilder();
+
+        for (int i = 1; i < args.length; i++) {
+            reason.append(args[i]).append(" ");
+        }
+
+        if (reason.toString().isEmpty()) {
+            reason.append(Messages.Default.defaultWarnReason());
+        }
+
         new Thread(() -> {
-            if (!(sender.hasPermission("mast.punishments"))) return;
-
-            if (args.length < 1) {
-                sender.sendMessage(Messages.Commands.Warn.usage());
-                return;
-            }
-
-            String target = args[0];
-
-            StringBuilder reason = new StringBuilder();
-
-            for (int i = 1; i < args.length; i++) {
-                reason.append(args[i]).append(" ");
-            }
-
-            if (reason.toString().isEmpty()) {
-                reason.append(Messages.Default.defaultWarnReason());
-            }
-            try {
-                UserModel senderUser = UserDataManager.getUserData(sender.getName());
-                UserModel targetUser = UserDataManager.getUserData(target);
-
-                WarnModel warnModel = new WarnModel();
-                warnModel.setWarnedBy(senderUser.getId());
-                warnModel.setUser(targetUser.getId());
-                warnModel.setReason(reason.toString());
-                warnModel.setTime(System.currentTimeMillis());
-                try {
-                    warnModel.setUntil(System.currentTimeMillis() + NumberUtils.parseToMilis(Config.Warn.expireAfter()));
-                } catch (Exception e) {
-                    warnModel.setUntil(-1L);
-                }
-
-                PluginConnection.getStorm().save(warnModel);
-
-            } catch (Exception e) {
-                MAStaffInstance.getLogger().debug("Error while getting user data: " + e.getMessage());
-                sender.sendMessage(Messages.Commands.playerNotFound(target));
-            }
-
-
+            sender.warn(args[0], reason.toString());
             sender.sendMessage(Messages.Commands.Warn.success(target, reason.toString(), sender.getName()));
         }).start();
     }
