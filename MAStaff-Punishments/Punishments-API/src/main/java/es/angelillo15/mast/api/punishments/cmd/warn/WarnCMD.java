61e7ec33ef6b1524f94a4eb799d95e1d4ab7a0a5
Angelillo15 <contact@angelillo15.es>
2023-07-03 20:55:55 +0200

    Deleted new thread

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/warn/WarnCMD.java
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/warn/WarnCMD.java
@@ -1,19 +1,9 @@
 package es.angelillo15.mast.api.punishments.cmd.warn;
 
-import es.angelillo15.mast.api.MAStaffInstance;
-import es.angelillo15.mast.api.cmd.Command;
 import es.angelillo15.mast.api.cmd.CommandData;
-import es.angelillo15.mast.api.cmd.sender.CommandSender;
-import es.angelillo15.mast.api.config.punishments.Config;
 import es.angelillo15.mast.api.config.punishments.Messages;
-import es.angelillo15.mast.api.database.PluginConnection;
-import es.angelillo15.mast.api.managers.UserDataManager;
-import es.angelillo15.mast.api.models.UserModel;
-import es.angelillo15.mast.api.models.WarnModel;
 import es.angelillo15.mast.api.punishments.IPunishPlayer;
-import es.angelillo15.mast.api.punishments.PunishPlayersManager;
 import es.angelillo15.mast.api.punishments.cmd.PunishCommand;
-import es.angelillo15.mast.api.utils.NumberUtils;
 
 @CommandData(
         name = "warn",
@@ -39,9 +29,6 @@ public class WarnCMD extends PunishCommand {
             reason.append(Messages.Default.defaultWarnReason());
         }
 
-        new Thread(() -> {
-            sender.warn(args[0], reason.toString());
-            sender.sendMessage(Messages.Commands.Warn.success(target, reason.toString(), sender.getName()));
-        }).start();
+        sender.warn(target, reason.toString());
     }
 }
