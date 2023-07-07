49afbeb6665ec4da5e094b2f288fcbe80bbcdb37
Angelillo15 <contact@angelillo15.es>
2023-07-07 19:20:44 +0200

    Added NoPlaceholderDataException

---
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/exceptions/NoPlaceholderDataException.java
@@ -0,0 +1,7 @@
+package es.angelillo15.mast.api.exceptions;
+
+public class NoPlaceholderDataException extends RuntimeException {
+    public NoPlaceholderDataException(String message) {
+        super(message);
+    }
+}
