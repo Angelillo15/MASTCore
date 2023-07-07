4d07b32523f61ef9a19721dec2eeaa5bf653c76b
Angelillo15 <contact@angelillo15.es>
2023-07-07 19:22:37 +0200

    Deleted unused lib reference

--- MAStaff-Vanish/build.gradle.kts
+++ MAStaff-Vanish/build.gradle.kts
@@ -7,12 +7,10 @@ version = parent?.version ?: "undefined"
 
 repositories {
     mavenCentral()
-    maven("https://repo.dmulloy2.net/repository/public/")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
     compileOnly(project(":MAStaff-API"))
-    compileOnly(libs.protocolLib)
     compileOnly(libs.spigot)
 }
