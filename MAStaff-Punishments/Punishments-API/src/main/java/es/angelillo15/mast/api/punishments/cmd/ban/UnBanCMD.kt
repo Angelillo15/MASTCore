0905ba8b2a6d91564bec69bdc5001af62a5cb0a0
Angelillo15 <contact@angelillo15.es>
2023-07-17 03:34:55 +0200

    Implement TargetReasonCommand

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/UnBanCMD.kt
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/UnBanCMD.kt
@@ -4,6 +4,7 @@ import es.angelillo15.mast.api.MAStaffInstance
 import es.angelillo15.mast.api.cmd.CommandData
 import es.angelillo15.mast.api.config.punishments.Messages
 import es.angelillo15.mast.api.punishments.IPunishPlayer
+import es.angelillo15.mast.api.punishments.PunishTargetReasonCommand
 import es.angelillo15.mast.api.punishments.cmd.PunishCommand
 
 @CommandData(
@@ -13,36 +14,18 @@ import es.angelillo15.mast.api.punishments.cmd.PunishCommand
         permission = "mast.punishments.unban",
         aliases = ["ub"]
 )
-class UnBanCMD : PunishCommand() {
-    override fun onCommand(sender: IPunishPlayer, label: String, args: Array<String>) {
-
+class UnBanCMD : PunishTargetReasonCommand(1, Messages.Default.defaultUnbanReason()) {
+    override fun onCommand(sender: IPunishPlayer, target: String, label: String, args: Array<out String>, reason: String) {
         if (args.isEmpty()) {
             sender.sendMessage(Messages.Commands.Unban.usage())
             return
         }
 
-        val reason = StringBuilder()
-
-        for (i in 1 until args.size) {
-            reason.append(args[i]).append(" ")
-        }
-
-        if (reason.toString().isEmpty()) {
-            reason.append(Messages.Default.defaultUnbanReason())
-        }
-
-        val target = args[0]
-
-        try {
-            sender.unban(target, reason.toString())
-        } catch (e: Exception) {
-            MAStaffInstance.getLogger().debug("Error while unbanning player " + target + ": " + e.message)
-            return
-        }
+        sender.unban(target, reason)
 
         sender.sendMessage(Messages.Commands.Unban.success(
                 target,
-                reason.toString(),
+                reason,
                 sender.name
         ))
     }
