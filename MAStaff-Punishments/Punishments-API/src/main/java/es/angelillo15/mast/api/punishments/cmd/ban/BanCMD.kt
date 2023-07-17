7b16c5fea03e84e39b03a7ac3ae04bc40109859a
Angelillo15 <contact@angelillo15.es>
2023-07-17 03:34:42 +0200

    Implement TargetReasonCommand

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/BanCMD.kt
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/BanCMD.kt
@@ -3,34 +3,23 @@ package es.angelillo15.mast.api.punishments.cmd.ban
 import es.angelillo15.mast.api.cmd.CommandData
 import es.angelillo15.mast.api.config.punishments.Messages
 import es.angelillo15.mast.api.punishments.IPunishPlayer
-import es.angelillo15.mast.api.punishments.cmd.PunishCommand
+import es.angelillo15.mast.api.punishments.PunishTargetReasonCommand
 
 @CommandData(
-        name = "ban",
-        description = "Ban a player",
-        usage = "/ban <player> <reason>",
-        permission = "mast.punishments.ban",
-        aliases = ["b"]
+    name = "ban",
+    description = "Ban a player",
+    usage = "/ban <player> <reason>",
+    permission = "mast.punishments.ban",
+    aliases = ["b"]
 )
-class BanCMD : PunishCommand() {
-    override fun onCommand(punishPlayer: IPunishPlayer, label: String, args: Array<String>) {
+class BanCMD : PunishTargetReasonCommand(1, Messages.Default.defaultBanReason()) {
+    override fun onCommand(sender: IPunishPlayer, target: String, label: String, args: Array<out String>, reason: String) {
         if (args.isEmpty()) {
-            punishPlayer.sendMessage(Messages.Commands.Ban.usage())
+            sender.sendMessage(Messages.Commands.Ban.usage())
             return
         }
 
-        val target = args[0]
-        val reason = StringBuilder()
-
-        for (i in 1 until args.size) {
-            reason.append(args[i]).append(" ")
-        }
-
-        if (reason.toString().isEmpty()) {
-            reason.append(Messages.Default.defaultBanReason())
-        }
-
-        punishPlayer.ban(args[0], reason.toString())
-        punishPlayer.sendMessage(Messages.Commands.Ban.success(target, reason.toString(), punishPlayer.name))
+        sender.ban(target, reason)
+        sender.sendMessage(Messages.Commands.Ban.success(target, reason, sender.name))
     }
 }
