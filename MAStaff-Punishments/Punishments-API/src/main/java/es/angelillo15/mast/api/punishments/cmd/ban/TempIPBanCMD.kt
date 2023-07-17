1afca154e52486a64b0d327998cb0ff8fc603526
Angelillo15 <contact@angelillo15.es>
2023-07-17 03:34:33 +0200

    Implement TargetReasonCommand

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/TempIPBanCMD.kt
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/ban/TempIPBanCMD.kt
@@ -5,31 +5,23 @@ import es.angelillo15.mast.api.cmd.CommandData
 import es.angelillo15.mast.api.config.punishments.Config
 import es.angelillo15.mast.api.config.punishments.Messages
 import es.angelillo15.mast.api.punishments.IPunishPlayer
+import es.angelillo15.mast.api.punishments.PunishTargetReasonCommand
 import es.angelillo15.mast.api.punishments.cmd.PunishCommand
 import es.angelillo15.mast.api.utils.NumberUtils
 
 @CommandData(
-        name = "tempipban",
-        permission = "mast.ban.tempipban",
-        aliases = ["tempipban", "tempipb"],
-        usage = "&cUsage: &7/tempipban <player> <time> <reason>",
-        description = "Temporarily IP ban a player"
+    name = "tempipban",
+    permission = "mast.ban.tempipban",
+    aliases = ["tempipban", "tempipb"],
+    usage = "&cUsage: &7/tempipban <player> <time> <reason>",
+    description = "Temporarily IP ban a player"
 )
-class TempIPBanCMD : PunishCommand() {
-    override fun onCommand(sender: IPunishPlayer, label: String, args: Array<String>) {
+class TempIPBanCMD : PunishTargetReasonCommand(2, Messages.Default.defaultBanReason()) {
+
+    override fun onCommand(sender: IPunishPlayer, target: String, label: String, args: Array<out String>, reason: String) {
         if (args.size < 2) {
             sender.sendMessage(Messages.Commands.TempIpBan.usage())
             return
-        }
-
-        val target = args[0]
-        val reason = StringBuilder()
-
-        for (i in 2 until args.size) {
-            reason.append(args[i]).append(" ")
-        }
-        if (reason.toString().isEmpty()) {
-            reason.append(Messages.Default.defaultBanReason())
         }
 
         val time: Long = try {
@@ -39,12 +31,15 @@ class TempIPBanCMD : PunishCommand() {
             return
         }
 
-        sender.ban(target, reason.toString(), time, true)
-        sender.sendMessage(Messages.Commands.TempIpBan.success(
+        sender.ban(target, reason, time, true)
+        sender.sendMessage(
+            Messages.Commands.TempIpBan.success(
                 target,
                 TextUtils.formatDate(time, Config.dateFormat()),
-                reason.toString(),
-                sender.name)
+                reason,
+                sender.name
+            )
         )
     }
+
 }
