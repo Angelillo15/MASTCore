7eff209bd07471ba846675c940dfd63fc7cb5f26
Angelillo15 <contact@angelillo15.es>
2023-07-18 15:15:27 +0200

    Init VanishThread

--- MAStaff-Vanish/src/main/java/es/angelillo15/mast/vanish/MAStaffVanish.java
+++ MAStaff-Vanish/src/main/java/es/angelillo15/mast/vanish/MAStaffVanish.java
@@ -15,6 +15,7 @@ public class MAStaffVanish extends MAStaffAddon<JavaPlugin> {
         getLogger().info("Enabling MAStaff Vanish module...");
         useProtocolLib = ServerUtils.isProtocolLibInstalled();
         registerListeners();
+        VanishThreadKt.init();
     }
 
     void registerListeners() {
