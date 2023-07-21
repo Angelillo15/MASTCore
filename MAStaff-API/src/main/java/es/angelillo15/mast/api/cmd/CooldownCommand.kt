8cfe4aa560257236acd1edb4b28b5abf04445fee
Angelillo15 <contact@angelillo15.es>
2023-07-21 18:55:22 +0200

    Fix CooldownCommand

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/cmd/CooldownCommand.kt
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/cmd/CooldownCommand.kt
@@ -3,11 +3,11 @@ package es.angelillo15.mast.api.cmd
 import es.angelillo15.mast.api.cmd.sender.CommandSender
 
 abstract class CooldownCommand(
-    val cooldown: Int = 60,
+    private val cooldown: Int = 60,
     private val cooldownMessage: String = "&cYou must wait &a{cooldown} &cseconds to use again the command"
 ) : Command() {
     private var cooldowns: MutableMap<CommandSender, Long> = mutableMapOf()
-    override fun onCommand(sender: CommandSender?, label: String?, args: Array<out String>?) {
+    override fun onCommand(sender: CommandSender?, label: String?, args: Array<String?>?) {
         if (cooldowns.containsKey(sender)) {
             sender!!.sendMessage(cooldownMessage.replace("{cooldown}", (cooldowns[sender]!! - System.currentTimeMillis() / 1000).toString()))
             return
@@ -18,5 +18,5 @@ abstract class CooldownCommand(
         onCooldownCommand(sender, label!!, args!!)
     }
 
-    abstract fun onCooldownCommand(sender: CommandSender, label: String, args: Array<out String>)
+    abstract fun onCooldownCommand(sender: CommandSender, label: String, args: Array<String?>)
 }
