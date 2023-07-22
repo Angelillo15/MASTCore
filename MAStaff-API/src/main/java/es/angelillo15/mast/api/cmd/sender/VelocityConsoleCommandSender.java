212cfc0c76ece9921afcbefa9d1ab9ed2face833
Angelillo15 <contact@angelillo15.es>
2023-07-22 15:20:27 +0200

    Added velocities command senders

---
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/cmd/sender/VelocityConsoleCommandSender.java
@@ -0,0 +1,4 @@
+package es.angelillo15.mast.api.cmd.sender;
+
+public class VelocityConsoleCommandSender extends BungeeConsoleCommandSender {
+}
---
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/cmd/sender/VelocityPlayerCommandSender.java
@@ -0,0 +1,62 @@
+package es.angelillo15.mast.api.cmd.sender;
+
+import com.velocitypowered.api.proxy.Player;
+import es.angelillo15.mast.api.TextUtils;
+
+public class VelocityPlayerCommandSender implements CommandSender {
+    private final Player player;
+
+    public VelocityPlayerCommandSender(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        player.sendMessage(TextUtils.toComponent(message));
+    }
+
+    @Override
+    public boolean hasPermission(String permission) {
+        return player.hasPermission(permission);
+    }
+
+    @Override
+    public String getName() {
+        return player.getUsername();
+    }
+
+    @Override
+    public String getUniqueId() {
+        return player.getUniqueId().toString();
+    }
+
+    @Override
+    public String getAddress() {
+        return player.getRemoteAddress().getAddress().getHostAddress().split(":")[0];
+    }
+
+    @Override
+    public boolean isPlayer() {
+        return true;
+    }
+
+    @Override
+    public boolean isConsole() {
+        return false;
+    }
+
+    @Override
+    public boolean isProxy() {
+        return true;
+    }
+
+    @Override
+    public boolean isBungee() {
+        return true;
+    }
+
+    @Override
+    public boolean isSpigot() {
+        return false;
+    }
+}
