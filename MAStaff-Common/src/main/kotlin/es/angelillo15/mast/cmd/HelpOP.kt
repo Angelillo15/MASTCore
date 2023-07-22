b018c3331a87da295b505964c3cec8b34dd9cb85
Angelillo15 <contact@angelillo15.es>
2023-07-22 15:19:52 +0200

    Create Helop command test

--- MAStaff-Common/src/main/kotlin/es/angelillo15/mast/cmd/HelpOP.kt
+++ MAStaff-Common/src/main/kotlin/es/angelillo15/mast/cmd/HelpOP.kt
@@ -1,10 +1,15 @@
 package es.angelillo15.mast.cmd
 
+import es.angelillo15.mast.api.cmd.CommandData
 import es.angelillo15.mast.api.cmd.CooldownCommand
 import es.angelillo15.mast.api.cmd.sender.CommandSender
 
+@CommandData(
+    name = "HelpOP",
+    permission = "mast.helpop",
+)
 class HelpOP : CooldownCommand(60) {
-    override fun onCooldownCommand(sender: CommandSender, label: String, args: Array<out String>) {
-        TODO("Not yet implemented")
+    override fun onCooldownCommand(sender: CommandSender, label: String, args: Array<String?>) {
+        sender.sendMessage("HelpOP command!")
     }
 }
