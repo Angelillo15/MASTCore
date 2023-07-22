f96b1c9a71d3b2277f562140cccdcc6fdf9bdf12
Angelillo15 <contact@angelillo15.es>
2023-07-22 15:19:34 +0200

    Added custom command class to velocity

---
+++ MAStaff-Velocity/src/main/java/es/angelillo15/mast/velocity/cmd/CustomCommand.kt
@@ -0,0 +1,39 @@
+package es.angelillo15.mast.velocity.cmd
+
+import com.google.inject.Inject
+import com.velocitypowered.api.command.SimpleCommand
+import com.velocitypowered.api.proxy.ConsoleCommandSource
+import com.velocitypowered.api.proxy.Player
+import es.angelillo15.mast.api.ILogger
+import es.angelillo15.mast.api.cmd.Command
+import es.angelillo15.mast.api.cmd.sender.CommandSender
+import es.angelillo15.mast.api.cmd.sender.VelocityConsoleCommandSender
+import es.angelillo15.mast.api.cmd.sender.VelocityPlayerCommandSender
+
+class CustomCommand(private var command: Command, private var permission: String?) : SimpleCommand {
+    @Inject
+    private lateinit var logger: ILogger;
+    override fun execute(invocation: SimpleCommand.Invocation?) {
+
+        val sender: CommandSender = if (invocation!!.source() is ConsoleCommandSource) {
+            VelocityConsoleCommandSender()
+        } else if (invocation.source() is Player) {
+            VelocityPlayerCommandSender(invocation.source() as Player)
+        } else {
+            logger.error("Unknown command sender: ${invocation.source()}")
+            return
+        }
+
+        command.onCommand(sender, invocation.alias(), invocation.arguments())
+    }
+
+    override fun hasPermission(invocation: SimpleCommand.Invocation?): Boolean {
+        if (permission.isNullOrBlank()) return true
+
+        if (invocation is Player) {
+            return invocation.hasPermission(permission)
+        }
+
+        return true
+    }
+}
