8fb0523a7ac63fc6226f54c4714845913164889d
Angelillo15 <contact@angelillo15.es>
2023-07-22 15:19:20 +0200

    added the logger to injector

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/inject/CommonModule.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/inject/CommonModule.java
@@ -1,11 +1,13 @@
 package es.angelillo15.mast.api.inject;
 
 import com.google.inject.AbstractModule;
+import es.angelillo15.mast.api.ILogger;
 import es.angelillo15.mast.api.managers.UserDataManager;
 
 public class CommonModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(UserDataManager.class).asEagerSingleton();
+        bind(ILogger.class).toInstance(ILogger.getInstance());
     }
 }
