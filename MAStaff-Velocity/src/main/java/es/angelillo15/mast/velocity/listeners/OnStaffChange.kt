5832845a7d5c2997192e9b64313dead07903e3ce
Angelillo15 <contact@angelillo15.es>
2023-07-25 09:59:52 +0200

    Added OnStaffChange listeners

---
+++ MAStaff-Velocity/src/main/java/es/angelillo15/mast/velocity/listeners/OnStaffChange.kt
@@ -0,0 +1,37 @@
+package es.angelillo15.mast.velocity.listeners
+
+import com.google.common.io.ByteStreams
+import com.google.inject.Inject
+import com.velocitypowered.api.event.Subscribe
+import com.velocitypowered.api.event.connection.PluginMessageEvent
+import es.angelillo15.mast.api.ILogger
+import es.angelillo15.mast.api.config.velocity.Messages
+
+class OnStaffChange {
+    @Inject
+    private lateinit var logger: ILogger
+
+    @Subscribe
+    fun staffChange(event: PluginMessageEvent) {
+        if (event.identifier.id != "BungeeCord") return
+
+        val input = ByteStreams.newDataInput(event.data)
+
+        val subChannel = input.readUTF()
+
+        if (subChannel != "mast") return
+
+        val player = input.readUTF()
+        val status = input.readBoolean()
+
+        if (status) {
+            logger.info(
+                Messages.playerStaffModeEnabled().replace("{player}", player)
+            )
+        } else {
+            logger.info(
+                Messages.playerStaffModeDisabled().replace("{player}", player)
+            )
+        }
+    }
+}
