53460d7a5337f1c72f2ac50740866c451125b82d
Angelillo15 <contact@angelillo15.es>
2023-07-25 09:32:05 +0200

    Inject Velocity config

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/config/velocity/Config.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/config/velocity/Config.java
@@ -1,33 +1,38 @@
 package es.angelillo15.mast.api.config.velocity;
 
+import com.google.inject.Inject;
+
 public class Config {
+    @Inject
+    private static VelocityConfig config;
+
     public static String dateFormat() {
-        return ConfigLoader.getConfig().getConfig().getString("Config.date-format");
+        return config.getConfig().getConfig().getString("Config.date-format");
     }
 
     public static class Database {
         public static String type() {
-            return ConfigLoader.getConfig().getConfig().getString("Database.type");
+            return config.getConfig().getConfig().getString("Database.type");
         }
 
         public static String host() {
-            return ConfigLoader.getConfig().getConfig().getString("Database.host");
+            return config.getConfig().getConfig().getString("Database.host");
         }
 
         public static int port() {
-            return ConfigLoader.getConfig().getConfig().getInt("Database.port");
+            return config.getConfig().getConfig().getInt("Database.port");
         }
 
         public static String database() {
-            return ConfigLoader.getConfig().getConfig().getString("Database.database");
+            return config.getConfig().getConfig().getString("Database.database");
         }
 
         public static String username() {
-            return ConfigLoader.getConfig().getConfig().getString("Database.user");
+            return config.getConfig().getConfig().getString("Database.user");
         }
 
         public static String password() {
-            return ConfigLoader.getConfig().getConfig().getString("Database.password");
+            return config.getConfig().getConfig().getString("Database.password");
         }
     }
 }
