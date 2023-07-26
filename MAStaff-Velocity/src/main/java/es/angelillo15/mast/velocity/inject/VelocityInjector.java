bddf53375e46d9aef41a0aa2f4b864e37dcadaf5
Angelillo15 <contact@angelillo15.es>
2023-07-26 19:56:49 +0200

    Added new fields to VelocityInjector

--- MAStaff-Velocity/src/main/java/es/angelillo15/mast/velocity/inject/VelocityInjector.java
+++ MAStaff-Velocity/src/main/java/es/angelillo15/mast/velocity/inject/VelocityInjector.java
@@ -1,11 +1,14 @@
 package es.angelillo15.mast.velocity.inject;
 
+import com.velocitypowered.api.proxy.ProxyServer;
+import es.angelillo15.mast.api.IServerUtils;
 import es.angelillo15.mast.api.MAStaffInstance;
 import es.angelillo15.mast.api.config.common.CommonConfig;
 import es.angelillo15.mast.api.config.velocity.VelocityConfig;
 import es.angelillo15.mast.api.inject.CommonModule;
 import es.angelillo15.mast.api.utils.MAStaffInject;
 import es.angelillo15.mast.velocity.MAStaff;
+import es.angelillo15.mast.velocity.utils.VelocityServerUtils;
 
 public class VelocityInjector extends CommonModule {
     @Override
@@ -16,5 +19,7 @@ public class VelocityInjector extends CommonModule {
         bind(MAStaff.class).toInstance(MAStaff.getInstance());
         bind(CommonConfig.class).asEagerSingleton();
         bind(VelocityConfig.class).asEagerSingleton();
+        bind(IServerUtils.class).to(VelocityServerUtils.class).asEagerSingleton();
+        bind(ProxyServer.class).toInstance(MAStaff.getInstance().getProxyServer());
     }
 }
