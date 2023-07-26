67832b37036478eeda63d2caf2c037088915edea
Angelillo15 <contact@angelillo15.es>
2023-07-26 19:56:24 +0200

    Using minimessage for console

--- MAStaff-Velocity/src/main/java/es/angelillo15/mast/velocity/utils/Logger.java
+++ MAStaff-Velocity/src/main/java/es/angelillo15/mast/velocity/utils/Logger.java
@@ -1,14 +1,17 @@
 package es.angelillo15.mast.velocity.utils;
 
+import com.velocitypowered.api.proxy.ProxyServer;
 import es.angelillo15.mast.api.ILogger;
 import es.angelillo15.mast.api.TextUtils;
 import es.angelillo15.mast.velocity.MAStaff;
 
 public class Logger extends ILogger {
     private final org.slf4j.Logger logger;
+    private final ProxyServer proxyServer;
 
     public Logger() {
         this.logger = MAStaff.getInstance().getSlf4jLogger();
+        this.proxyServer = MAStaff.getInstance().getProxyServer();
         setInstance(this);
     }
 
@@ -16,7 +19,7 @@ public class Logger extends ILogger {
 
     @Override
     public void info(String message) {
-        logger.info(TextUtils.simpleColorize(message));
+        proxyServer.getConsoleCommandSource().sendMessage(TextUtils.toComponent(prefix + message));
     }
 
     @Override
