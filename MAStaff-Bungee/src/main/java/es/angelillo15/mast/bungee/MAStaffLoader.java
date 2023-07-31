178d9c9473ea651921b38edeca900f40b26e9ee2
Angelillo15 <contact@angelillo15.es>
2023-07-31 03:49:02 +0200

    Set audience

--- MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/MAStaffLoader.java
+++ MAStaff-Bungee/src/main/java/es/angelillo15/mast/bungee/MAStaffLoader.java
@@ -2,6 +2,7 @@ package es.angelillo15.mast.bungee;
 
 import es.angelillo15.mast.api.database.PluginConnection;
 import es.angelillo15.mast.api.managers.loader.ReflectionLoader;
+import es.angelillo15.mast.api.utils.BungeeUtils;
 import es.angelillo15.mast.bungee.addons.AddonsLoader;
 import es.angelillo15.mast.bungee.metrics.Metrics;
 import es.angelillo15.mast.bungee.utils.LibsLoader;
@@ -14,6 +15,7 @@ public class MAStaffLoader extends MAStaff {
         super.onEnable();
         drawLogo();
         LibsLoader.loadLibs();
+        BungeeUtils.setAudienceBungee(this);
         loadInjector();
         loadConfig();
         loadDatabase();
