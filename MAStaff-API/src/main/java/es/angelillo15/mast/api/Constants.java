45c0e1b952458bcae3651ced7f40c488af651154
Angelillo15 <contact@angelillo15.es>
2023-08-01 13:16:43 +0200

    Fix version in CommandParent

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/Constants.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/Constants.java
@@ -3,14 +3,15 @@ package es.angelillo15.mast.api;
 import lombok.Getter;
 
 public class Constants {
-    @Getter
     public static final String VERSION = "{version}";
     public static final String COMMIT = "{git-commit}";
     public static final String COMMIT_USER = "{git-user}";
     public static final String COMMIT_TIME = "{git-date}";
     public static final String GIT_BRANCH = "{git-branch}";
     public static final boolean DEV_MODE = false;
-
+    public static String getVersion() {
+        return VERSION;
+    }
     private Constants() {}
 
 
