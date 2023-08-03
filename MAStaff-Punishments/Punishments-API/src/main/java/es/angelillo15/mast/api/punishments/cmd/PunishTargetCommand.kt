fc3a8a252f2ae2012fa393270c4590d7f6b01cd8
Angelillo15 <contact@angelillo15.es>
2023-08-04 01:46:16 +0200

    Fix null target command

--- MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/PunishTargetCommand.kt
+++ MAStaff-Punishments/Punishments-API/src/main/java/es/angelillo15/mast/api/punishments/cmd/PunishTargetCommand.kt
@@ -4,7 +4,13 @@ import es.angelillo15.mast.api.punishments.IPunishPlayer
 
 abstract class PunishTargetCommand : PunishCommand() {
     override fun onCommand(sender: IPunishPlayer?, label: String?, args: Array<out String>) {
-        onCommand(sender!!, args[0], label!!, args)
+        val argsParsed = if (args.isEmpty()) {
+            ""
+        } else {
+            args[0]
+        }
+
+        onCommand(sender!!, argsParsed, label!!, args)
     }
 
     abstract fun onCommand(sender: IPunishPlayer, target: String, label: String, args:  Array<out String>)
