e6437d8b28db9ed03fb9fd133bfdbc5c1033eb9d
Angelillo15 <contact@angelillo15.es>
2023-08-04 16:39:29 +0200

    Added warn duration

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/templates/WarnTemplate.kt
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/templates/WarnTemplate.kt
@@ -2,10 +2,11 @@ package es.angelillo15.mast.api.templates
 
 data class WarnTemplate(
     val id: String,
+    val warnDuration: Long,
     val warnReason: String,
     val warnMessage: String,
     val maxWarnings: Int,
-    val actions: List<WarnAction>,
+    val actions: List<WarnAction?>,
     val deleteOnMax: Boolean,
     val permission: String
 )
