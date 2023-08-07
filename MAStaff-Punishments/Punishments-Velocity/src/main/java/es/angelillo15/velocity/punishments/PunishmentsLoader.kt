68a83891b75a7f87408e0221cc20bc861c8033d2
Angelillo15 <contact@angelillo15.es>
2023-08-07 22:57:46 +0200

    Added test class

---
+++ MAStaff-Punishments/Punishments-Velocity/src/main/java/es/angelillo15/velocity/punishments/PunishmentsLoader.kt
@@ -0,0 +1,13 @@
+package es.angelillo15.velocity.punishments
+
+import es.angelillo15.mast.api.Constants
+import es.angelillo15.mast.api.addons.annotations.Addon
+
+@Addon(
+    name = "MAStaff-Punishments",
+    version = Constants.VERSION,
+    author = "Angelillo15",
+    description = "An addon for MAStaff that adds punishments to the plugin",
+)
+class PunishmentsLoader : MAStaffPunishments() {
+}
