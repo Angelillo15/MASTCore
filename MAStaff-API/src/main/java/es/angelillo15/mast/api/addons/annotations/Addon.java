96665d4e0b0ac4b02abd886523fe190d92fb78d1
Angelillo15 <contact@angelillo15.es>
2023-08-07 22:55:38 +0200

    Added new Addon interface to scan at the runtime

---
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/addons/annotations/Addon.java
@@ -0,0 +1,8 @@
+package es.angelillo15.mast.api.addons.annotations;
+
+public @interface Addon {
+    String name();
+    String version();
+    String author();
+    String description() default "";
+}
