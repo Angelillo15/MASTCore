180c9b32d588f944879ab01c2b1f4d2f85eb73e7
Angelillo15 <contact@angelillo15.es>
2023-09-08 16:59:08 +0200

    Fix permission

--- MAStaff-Velocity/src/main/java/es/angelillo15/mast/velocity/cmd/CustomCommand.kt
+++ MAStaff-Velocity/src/main/java/es/angelillo15/mast/velocity/cmd/CustomCommand.kt
@@ -10,7 +10,7 @@ import es.angelillo15.mast.api.cmd.sender.CommandSender
 import es.angelillo15.mast.api.cmd.sender.VelocityConsoleCommandSender
 import es.angelillo15.mast.api.cmd.sender.VelocityPlayerCommandSender
 
-class CustomCommand(private var command: Command, private var permission: String?) : SimpleCommand {
+class CustomCommand(private var command: Command, private var permission: String) : SimpleCommand {
   @Inject
   private lateinit var logger: ILogger;
   override fun execute(invocation: SimpleCommand.Invocation?) {
@@ -22,21 +22,13 @@ class CustomCommand(private var command: Command, private var permission: String
       logger.error("Unknown command sender: ${invocation.source()}")
       return
     }
-    if (!hasPermission(invocation)) {
-      return
-    }
 
-    command.onCommand(sender, invocation.alias(), invocation.arguments())
+    if (invocation.source().hasPermission(permission)) {
+      command.onCommand(sender, invocation.alias(), invocation.arguments())
+    }
   }
 
-  override fun hasPermission(invocation: SimpleCommand.Invocation?): Boolean {
-    if (permission.isNullOrBlank()) return true
-
-    if (invocation == null) {
-      return false
-    }
-
-    invocation.source().hasPermission(permission)
-    return true
+  override fun hasPermission(invocation: SimpleCommand.Invocation): Boolean {
+    return invocation.source().hasPermission(permission)
   }
 }
