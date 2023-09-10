c9a464d062b8f1bf061a9d7994527694c443c5d8
Angelillo15 <contact@angelillo15.es>
2023-09-10 14:48:44 +0200

    Added VersionSupport

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/inject/BukkitInjector.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/inject/BukkitInjector.java
@@ -1,11 +1,13 @@
 package es.angelillo15.mast.bukkit.inject;
 
+import es.angelillo15.mast.bukkit.nms.VersionSupport;
 import es.angelillo15.mast.api.MAStaffInstance;
 import es.angelillo15.mast.api.inject.CommonModule;
 import es.angelillo15.mast.api.managers.StaffManager;
 import es.angelillo15.mast.api.utils.MAStaffInject;
 import es.angelillo15.mast.bukkit.MAStaff;
 import es.angelillo15.mast.bukkit.ServerUtils;
+import es.angelillo15.mast.bukkit.utils.NMSUtils;
 
 public class BukkitInjector extends CommonModule {
   @Override
@@ -16,5 +18,6 @@ public class BukkitInjector extends CommonModule {
     bind(MAStaffInject.class).toInstance(MAStaff.getPlugin());
     bind(StaffManager.class).asEagerSingleton();
     bind(ServerUtils.class).asEagerSingleton();
+    bind(VersionSupport.class).toInstance(NMSUtils.getVersionSupport());
   }
 }
