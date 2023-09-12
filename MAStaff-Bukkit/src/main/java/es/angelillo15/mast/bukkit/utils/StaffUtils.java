701a7fdcce250313b906dc087f0807a3b2e97993
Angelillo15 <contact@angelillo15.es>
2023-09-12 23:46:34 +0200

    Reuse random object

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/utils/StaffUtils.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/utils/StaffUtils.java
@@ -6,9 +6,11 @@ import es.angelillo15.mast.api.config.bukkit.Messages;
 import es.angelillo15.mast.bukkit.MAStaff;
 import es.angelillo15.mast.bukkit.utils.scheduler.Scheduler;
 import io.papermc.lib.PaperLib;
+
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Random;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -16,63 +18,68 @@ import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 public class StaffUtils {
-    public static void playerRandomTeleport(Player player) {
-        ArrayList<Player> players = new ArrayList<>();
-        Bukkit.getOnlinePlayers().forEach(p -> {
-            if (!(p.hasPermission(Permissions.STAFF.getPermission()))) {
-                players.add(p);
-            }
-        });
-        if (players.isEmpty()) {
-            TextUtils.sendMessage(player, Messages.GET_NO_PLAYER_ONLINE_MESSAGE());
-            return;
+  private static final Random random = new Random();
+  public static void playerRandomTeleport(Player player) {
+    ArrayList<Player> players = new ArrayList<>();
+
+    Bukkit.getOnlinePlayers().forEach(p -> {
+      if (!(p.hasPermission(Permissions.STAFF.getPermission()))) {
+        players.add(p);
+      }
+    });
+
+    if (players.isEmpty()) {
+      TextUtils.sendMessage(player, Messages.GET_NO_PLAYER_ONLINE_MESSAGE());
+      return;
+    }
+
+    PaperLib.teleportAsync(player, players.get(random.nextInt(players.size())).getLocation());
+  }
+
+  public static boolean passThrough(Player player, Location clickedLocation) {
+    Vector direction = player.getLocation().getDirection().normalize();
+
+    int distance = 0;
+    do {
+      clickedLocation.add(direction);
+      distance++;
+    } while (clickedLocation.getBlock().getType().equals(Material.AIR) && distance < 6);
+
+    PaperLib.teleportAsync(player, clickedLocation);
+    return true;
+  }
+
+  public static void asyncStaffBroadcastMessage(String message) {
+    Scheduler.executeAsync(() -> {
+      Bukkit.getOnlinePlayers().forEach(p -> {
+        if (p.hasPermission("mast.staff")) {
+          TextUtils.sendMessage(p, message);
         }
-        final Random random = new Random();
-        PaperLib.teleportAsync(player, players.get(random.nextInt(players.size())).getLocation());
-    }
+      });
+      MAStaff.getPlugin().getPLogger().info(message);
+    });
+  }
 
-    public static boolean passThrough(Player player, Location clickedLocation) {
-        Vector direction = player.getLocation().getDirection().normalize();
-        int distance = 0;
-        do {
-            clickedLocation.add(direction);
-            distance++;
-        } while (clickedLocation.getBlock().getType().equals(Material.AIR) && distance < 6);
-        PaperLib.teleportAsync(player, clickedLocation);
-        return true;
-    }
+  public static void asyncBroadcastMessage(String message) {
+    if (Objects.equals(message, "")) return;
 
-    public static void asyncStaffBroadcastMessage(String message) {
-        Scheduler.executeAsync(() -> {
-            Bukkit.getOnlinePlayers().forEach(p -> {
-                if (p.hasPermission("mast.staff")) {
-                    TextUtils.sendMessage(p, message);
-                }
-            });
-            MAStaff.getPlugin().getPLogger().info(message);
-        });
-    }
+    Scheduler.executeAsync(() -> {
+      Bukkit.getOnlinePlayers().forEach(p -> {
+        TextUtils.sendMessage(p, message);
+      });
+      MAStaff.getPlugin().getPLogger().info(message);
+    });
+  }
 
-    public static void asyncBroadcastMessage(String message) {
-        if (Objects.equals(message, "")) return;
-
-        Scheduler.executeAsync(() -> {
-            Bukkit.getOnlinePlayers().forEach(p -> {
-                TextUtils.sendMessage(p, message);
-            });
-            MAStaff.getPlugin().getPLogger().info(message);
-        });
-    }
-
-    public static void asyncStaffChatMessage(String message) {
-        Scheduler.executeAsync(() -> {
-            Bukkit.getOnlinePlayers().forEach(p -> {
-                if (p.hasPermission("mast.staffchat")) {
-                    TextUtils.sendMessage(p, message);
-                }
-            });
-            MAStaff.getPlugin().getPLogger().info(message);
-        });
-    }
+  public static void asyncStaffChatMessage(String message) {
+    Scheduler.executeAsync(() -> {
+      Bukkit.getOnlinePlayers().forEach(p -> {
+        if (p.hasPermission("mast.staffchat")) {
+          TextUtils.sendMessage(p, message);
+        }
+      });
+      MAStaff.getPlugin().getPLogger().info(message);
+    });
+  }
 }
 
