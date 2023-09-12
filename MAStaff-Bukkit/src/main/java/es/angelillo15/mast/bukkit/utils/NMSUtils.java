32e3c0034b2d1af2ed723a2b7ec8ed48261dea3c
Angelillo15 <contact@angelillo15.es>
2023-09-12 14:38:17 +0200

    Added new versions to version utils

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/utils/NMSUtils.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/utils/NMSUtils.java
@@ -1,7 +1,10 @@
 package es.angelillo15.mast.bukkit.utils;
 
-import es.angelillo15.mast.bukkit.nms.V1_20_1_R0;
-import es.angelillo15.mast.bukkit.nms.VersionSupport;
+import es.angelillo15.bukkit.nms.V1_12_2_R0;
+import es.angelillo15.bukkit.nms.V1_8_8_R0;
+import es.angelillo15.mast.bukkit.MAStaff;
+import es.angelillo15.mast.api.nms.VersionSupport;
+import es.angelillo15.mast.bukkit.nms.*;
 import org.bukkit.Bukkit;
 
 public class NMSUtils {
@@ -16,10 +19,15 @@ public class NMSUtils {
     if (versionSupport != null) {
       return versionSupport;
     }
-
     return switch (VERSION) {
-      case "v1_20_R1" -> versionSupport = new V1_20_1_R0();
-      default -> throw new RuntimeException("Your server version is not supported by MAStaff");
+      case "v1_20_R1" -> versionSupport = new V1_20_1_R0(MAStaff.getPlugin());
+      case "v1_19_R3" -> versionSupport = new V1_19_4_R0(MAStaff.getPlugin());
+      case "v1_18_R2" -> versionSupport = new V1_18_2_R0(MAStaff.getPlugin());
+      case "v1_17_R1" -> versionSupport = new V1_17_1_R0(MAStaff.getPlugin());
+      case "v1_16_R3" -> versionSupport = new V1_16_5_R0(MAStaff.getPlugin());
+      case "v1_12_R1" -> versionSupport = new V1_12_2_R0(MAStaff.getPlugin());
+      case "v1_8_R3" -> versionSupport = new V1_8_8_R0(MAStaff.getPlugin());
+      default -> throw new RuntimeException("Your server version " + VERSION + " is not supported by MAStaff");
     };
   }
 
