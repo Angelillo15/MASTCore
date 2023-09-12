88285a3600cabd8e8c7face9d9a94eeca5fbd191
Angelillo15 <contact@angelillo15.es>
2023-09-12 14:56:45 +0200

    Delete without sense checks

--- MAStaff-Vanish/src/main/java/es/angelillo15/mast/vanish/VanishPlayer.java
+++ MAStaff-Vanish/src/main/java/es/angelillo15/mast/vanish/VanishPlayer.java
@@ -1,54 +1,45 @@
 package es.angelillo15.mast.vanish;
 
-import com.google.inject.Inject;
-import es.angelillo15.mast.bukkit.nms.VersionSupport;
 import es.angelillo15.mast.api.IStaffPlayer;
 import es.angelillo15.mast.api.factory.StaffPlayerFactory;
 import es.angelillo15.mast.api.player.IVanishPlayer;
-import es.angelillo15.mast.api.vanish.VanishDataManager;
 import es.angelillo15.mast.vanish.listeners.VanishListener;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class VanishPlayer implements IVanishPlayer, StaffPlayerFactory<VanishPlayer> {
-  @Inject
-  private VersionSupport versionSupport;
 
   private IStaffPlayer player;
 
   @Override
   public void enableVanish() {
-    VanishDataManager.addVanishedPlayer(player);
-
-    for (Player p : Bukkit.getOnlinePlayers()) {
-      if (p == player.getPlayer()) {
-        continue;
-      }
-      
-      if (p.hasPermission("mast.vanish.see")) {
-        continue;
-      }
-
-      versionSupport.sendVanishOnPackets(player.getPlayer(), p);
-    }
-  }
-
-  @Override
-  public void disableVanish() {
-    VanishDataManager.removeVanishedPlayer(player);
-
     Bukkit.getOnlinePlayers().forEach(p -> {
       if (p == player.getPlayer()) {
         return;
       }
 
-      versionSupport.sendVanishOffPackets(player.getPlayer(), p);
+      if (p.hasPermission("mast.vanish.see")) {
+        return;
+      }
+
+      hide(p);
+    });
+  }
+
+  @Override
+  public void disableVanish() {
+    Bukkit.getOnlinePlayers().forEach(p -> {
+      if (p == player.getPlayer()) {
+        return;
+      }
+
+      show(p);
     });
   }
 
   @Override
   public boolean isVanished() {
-    return VanishDataManager.isVanished(player);
+    return player.isVanished();
   }
 
   public void hide(Player player) {
