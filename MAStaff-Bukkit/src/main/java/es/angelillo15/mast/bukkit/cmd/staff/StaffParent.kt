253d9c0637f0a0eabd254cd686f1ef4ce78e6c50
Angelillo15 <contact@angelillo15.es>
2023-09-13 12:49:45 +0200

    Create new StaffParent based on CommandParent

---
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/cmd/staff/StaffParent.kt
@@ -0,0 +1,66 @@
+package es.angelillo15.mast.bukkit.cmd.staff
+
+import com.google.inject.Inject
+import es.angelillo15.mast.api.TextUtils
+import es.angelillo15.mast.api.cmd.CommandData
+import es.angelillo15.mast.api.cmd.CommandParent
+import es.angelillo15.mast.api.cmd.sender.CommandSender
+import es.angelillo15.mast.api.utils.MAStaffInject
+import es.angelillo15.mast.bukkit.cmd.staff.args.Punishments
+import es.angelillo15.mast.bukkit.cmd.staff.args.StaffList
+import es.angelillo15.mast.bukkit.cmd.staff.args.StaffMode
+import es.angelillo15.mast.bukkit.cmd.staff.args.StaffVault
+
+@CommandData(
+  name = "staff",
+  aliases = ["mod", "modmode", "staffmode"],
+  permission = "mast.staff"
+)
+class StaffParent : CommandParent() {
+  @Inject
+  private lateinit var inject: MAStaffInject;
+
+  override fun onCommand(sender: CommandSender, label: String, args: Array<String>) {
+    if (subCommands.isEmpty())
+      registerSubCommands()
+
+    if (sender.isConsole) {
+      sender.sendMessage("<red>Only players can execute this command.")
+      return
+    }
+
+    val subCommand = if (args.isEmpty()) {
+      getSubCommand("mode")
+    } else {
+      getSubCommand(args[0])
+    }
+
+    if (subCommand == null) {
+      sendHelp(sender)
+      return
+    }
+
+    if (!sender.hasPermission(subCommand.permission)) {
+      sender.sendMessage(TextUtils.simpleColorize(noPermission))
+      return
+    }
+
+    subCommand.onCommand(sender, label, args)
+  }
+
+  override fun registerSubCommands() {
+    registerHelpSubCommand("/staff")
+    registerSubCommand(inject.injector.getInstance(Punishments::class.java))
+    registerSubCommand(inject.injector.getInstance(StaffList::class.java))
+    registerSubCommand(inject.injector.getInstance(StaffVault::class.java))
+    registerSubCommand(inject.injector.getInstance(StaffMode::class.java))
+  }
+
+  override fun onTabComplete(sender: CommandSender, args: Array<String>): List<String> {
+    if (args.size == 1) {
+      return subCommands.keys.toList().requireNoNulls().filter { it.startsWith(args[0]) }
+    }
+
+    return emptyList()
+  }
+}
