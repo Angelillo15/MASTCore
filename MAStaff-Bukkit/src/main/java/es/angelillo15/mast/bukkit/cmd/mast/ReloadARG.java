8894a7776ddcddb784a621d0fc55a1bb366e7ec4
Angelillo15 <contact@angelillo15.es>
2023-09-13 13:31:43 +0200

    Change LegacySubCommand -> SubCommand

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/cmd/mast/ReloadARG.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/cmd/mast/ReloadARG.java
@@ -1,34 +1,35 @@
 package es.angelillo15.mast.bukkit.cmd.mast;
 
-import es.angelillo15.mast.api.cmd.LegacySubCommand;
+import es.angelillo15.mast.api.cmd.SubCommand;
+import es.angelillo15.mast.api.cmd.sender.CommandSender;
 import es.angelillo15.mast.api.config.bukkit.Messages;
 import es.angelillo15.mast.bukkit.MAStaff;
-import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
-public class ReloadARG extends LegacySubCommand {
-    @Override
-    public String getName() {
-        return "reload";
-    }
+public class ReloadARG extends SubCommand {
+  @Override
+  public String getName() {
+    return "reload";
+  }
 
-    @Override
-    public String getDescription() {
-        return "Reloads the plugin";
-    }
+  @Override
+  public String getDescription() {
+    return "Reloads the plugin";
+  }
 
-    @Override
-    public String getSyntax() {
-        return "/mast reload";
-    }
+  @Override
+  public String getSyntax() {
+    return "/mast reload";
+  }
 
-    @Override
-    public String getPermission() {
-        return "mast.reload";
-    }
+  @Override
+  public String getPermission() {
+    return "mast.reload";
+  }
 
-    @Override
-    public void execute(CommandSender sender, String[] args) {
-        MAStaff.getPlugin().reload();
-        sender.sendMessage(Messages.GET_RELOADED_MESSAGE());
-    }
+  @Override
+  public void onCommand(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) {
+    MAStaff.getPlugin().reload();
+    sender.sendMessage(Messages.GET_RELOADED_MESSAGE());
+  }
 }
