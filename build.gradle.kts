d7afd4d72deebdad539e45a5f2f917a4b99ddc0b
Angelillo15 <contact@angelillo15.es>
2023-09-13 12:46:21 +0200

    Change version to 3.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "es.angelillo15"
-version = "2.4.2"
+version = "3.0.0-BETA"
 
 tasks.shadowJar {
   relocate("es.angelillo15.configmanager", "es.angelillo15.mast.libs.config.manager")
