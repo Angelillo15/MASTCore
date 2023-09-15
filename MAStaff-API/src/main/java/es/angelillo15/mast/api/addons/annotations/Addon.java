b2d067ab4f74dc869c069c7e1d86cf4fbdc564b7
Angelillo15 <contact@angelillo15.es>
2023-09-15 22:25:44 +0200

    Refactor tab size from 4 -> 2

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/addons/annotations/Addon.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/addons/annotations/Addon.java
@@ -1,16 +1,21 @@
 package es.angelillo15.mast.api.addons.annotations;
 
 public @interface Addon {
-    String name();
-    String version();
-    String author();
-    String description() default "";
-    boolean loadOnScan() default true;
-    AddonPlatform platform();
+  String name();
 
-    enum AddonPlatform {
-        BUKKIT,
-        VELOCITY,
-        BUNGEECORD,
-    }
+  String version();
+
+  String author();
+
+  String description() default "";
+
+  boolean loadOnScan() default true;
+
+  AddonPlatform platform();
+
+  enum AddonPlatform {
+    BUKKIT,
+    VELOCITY,
+    BUNGEECORD,
+  }
 }
