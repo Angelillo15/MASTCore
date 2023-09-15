e95b4edacb2663c05be6aa89dfede176323a54d0
Angelillo15 <contact@angelillo15.es>
2023-09-15 21:59:01 +0200

    Refactor spaces

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/inject/CommonModule.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/inject/CommonModule.java
@@ -2,18 +2,19 @@ package es.angelillo15.mast.api.inject;
 
 import com.google.inject.AbstractModule;
 import es.angelillo15.mast.api.ILogger;
+import es.angelillo15.mast.api.managers.CommandSenderManager;
 import es.angelillo15.mast.api.managers.PreviousServerManager;
 import es.angelillo15.mast.api.managers.UserDataManager;
 import es.angelillo15.mast.api.templates.managers.BanTemplatesManager;
 import es.angelillo15.mast.api.templates.managers.WarnTemplateManager;
 
 public class CommonModule extends AbstractModule {
-    @Override
-    protected void configure() {
-        bind(UserDataManager.class).asEagerSingleton();
-        bind(ILogger.class).toInstance(ILogger.getInstance());
-        bind(PreviousServerManager.class).asEagerSingleton();
-        bind(WarnTemplateManager.class).asEagerSingleton();
-        bind(BanTemplatesManager.class).asEagerSingleton();
-    }
+  @Override
+  protected void configure() {
+    bind(UserDataManager.class).asEagerSingleton();
+    bind(ILogger.class).toInstance(ILogger.getInstance());
+    bind(PreviousServerManager.class).asEagerSingleton();
+    bind(WarnTemplateManager.class).asEagerSingleton();
+    bind(BanTemplatesManager.class).asEagerSingleton();
+  }
 }
