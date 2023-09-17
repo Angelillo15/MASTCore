dd2288f4dc39295890e4b3f311af52bb63520d2e
Angelillo15 <contact@angelillo15.es>
2023-09-17 13:47:29 +0200

    Create test addon with test listener

---
+++ MAStaff-API/src/test/java/com/nookure/mast/tests/addon/TestListener.java
@@ -0,0 +1,11 @@
+package com.nookure.mast.tests.addon;
+
+import com.nookure.mast.api.event.EventPriority;
+import com.nookure.mast.api.event.MastSubscribe;
+
+public class TestListener {
+  @MastSubscribe(priority = EventPriority.LOW)
+  public void onTestEvent(TestEvent event) {
+    System.out.println(event.getText());
+  }
+}
