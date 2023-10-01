2668d64103d6f9d6abd766fe6f728ef5d60523e5
Angelillo15 <contact@angelillo15.es>
2023-10-01 13:09:33 +0200

    Added StaffEvents

---
+++ MAStaff-API/src/main/java/com/nookure/mast/api/event/staff/StaffModeDisabledEvent.java
@@ -0,0 +1,16 @@
+package com.nookure.mast.api.event.staff;
+
+import com.nookure.mast.api.event.Event;
+import es.angelillo15.mast.api.IStaffPlayer;
+
+public class StaffModeDisabledEvent extends Event {
+  private final IStaffPlayer staffPlayer;
+
+  public StaffModeDisabledEvent(IStaffPlayer staffPlayer) {
+    this.staffPlayer = staffPlayer;
+  }
+
+  public IStaffPlayer getStaffPlayer() {
+    return staffPlayer;
+  }
+}
