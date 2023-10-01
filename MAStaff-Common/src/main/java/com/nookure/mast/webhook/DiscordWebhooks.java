c635ba2475313a3f4794c283d7a7b0392d9967c9
Angelillo15 <contact@angelillo15.es>
2023-10-01 14:32:53 +0200

    load default config

--- MAStaff-Common/src/main/java/com/nookure/mast/webhook/DiscordWebhooks.java
+++ MAStaff-Common/src/main/java/com/nookure/mast/webhook/DiscordWebhooks.java
@@ -5,9 +5,14 @@ import com.google.inject.Injector;
 import com.nookure.mast.api.addons.AddonActions;
 import com.nookure.mast.api.addons.annotations.Addon;
 import com.nookure.mast.api.addons.annotations.AddonLogger;
+import com.nookure.mast.api.config.ConfigurationContainer;
+import com.nookure.mast.webhook.config.WebhookConfig;
 import com.nookure.mast.webhook.json.WebhookLoader;
 import es.angelillo15.mast.api.Constants;
 import es.angelillo15.mast.api.ILogger;
+
+import java.io.File;
+import java.io.IOException;
 
 @Addon(
     name = "Discord Webhooks",
@@ -22,16 +27,30 @@ public class DiscordWebhooks implements AddonActions {
   @Inject
   @AddonLogger
   private ILogger logger;
+  @Inject
+  private File dataFolder;
   private Injector addonInjector;
+  private ConfigurationContainer<WebhookConfig> config;
 
   @Override
   public void onEnable() {
     logger.info("Loading Discord Webhooks...");
-    addonInjector = injector.createChildInjector(new WebhookInjector());
+    loadConfig();
+
+    addonInjector = injector.createChildInjector(new WebhookInjector(config.get()));
     loadWebhooksFiles();
   }
 
   public void loadWebhooksFiles() {
     addonInjector.getInstance(WebhookLoader.class).loadWebhooks();
+  }
+
+  public void loadConfig() {
+    try {
+      config = ConfigurationContainer.load(dataFolder.toPath(), WebhookConfig.class);
+      config.reload();
+    } catch (IOException e) {
+      logger.error("Could not load config.conf file", e);
+    }
   }
 }
