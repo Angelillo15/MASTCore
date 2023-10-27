348c253735aa54ff338d55c010ac02f6e11e7d25
Angelillo15 <contact@angelillo15.es>
2023-10-27 19:13:21 +0200

    register all listeners

--- MAStaff-Common/src/main/java/com/nookure/mast/webhook/DiscordWebhooks.java
+++ MAStaff-Common/src/main/java/com/nookure/mast/webhook/DiscordWebhooks.java
@@ -10,7 +10,14 @@ import com.nookure.mast.api.addons.annotations.AddonLogger;
 import com.nookure.mast.api.config.ConfigurationContainer;
 import com.nookure.mast.api.webhook.WebHookClient;
 import com.nookure.mast.webhook.config.WebhookConfig;
-import com.nookure.mast.webhook.handler.StaffChatListener;
+import com.nookure.mast.webhook.handler.staff.OnStaffChatMessage;
+import com.nookure.mast.webhook.handler.staff.OnStaffJoin;
+import com.nookure.mast.webhook.handler.staff.OnStaffLeave;
+import com.nookure.mast.webhook.handler.staff.OnStaffSwitch;
+import com.nookure.mast.webhook.handler.freeze.OnPlayerFreeze;
+import com.nookure.mast.webhook.handler.freeze.OnPlayerUnfreeze;
+import com.nookure.mast.webhook.handler.staff.OnStaffModeDisabled;
+import com.nookure.mast.webhook.handler.staff.OnStaffModeEnabled;
 import com.nookure.mast.webhook.json.WebhookLoader;
 import es.angelillo15.mast.api.Constants;
 import es.angelillo15.mast.api.ILogger;
@@ -59,9 +66,57 @@ public class DiscordWebhooks implements AddonActions {
   public void loadListeners() {
     if (config.get().staffChat.enabled()) {
       addonManager.registerListener(addonInjector
-          .getInstance(StaffChatListener.class)
-          .create(WebHookClient.fromURL(
-              config.get().staffChat.url())), addonContainer
+          .getInstance(OnStaffChatMessage.class)
+          .create(WebHookClient.fromURL(config.get().staffChat.url())), addonContainer
+      );
+    }
+
+    if (config.get().freeze.enabled()) {
+      addonManager.registerListener(addonInjector
+          .getInstance(OnPlayerFreeze.class)
+          .create(WebHookClient.fromURL(config.get().freeze.url())), addonContainer
+      );
+    }
+
+    if (config.get().unfreeze.enabled()) {
+      addonManager.registerListener(addonInjector
+          .getInstance(OnPlayerUnfreeze.class)
+          .create(WebHookClient.fromURL(config.get().unfreeze.url())), addonContainer
+      );
+    }
+
+    if (config.get().serverSwitch.enabled()) {
+      addonManager.registerListener(addonInjector
+          .getInstance(OnStaffSwitch.class)
+          .create(WebHookClient.fromURL(config.get().serverSwitch.url())), addonContainer
+      );
+    }
+
+    if (config.get().staffDisabled.enabled()) {
+      addonManager.registerListener(addonInjector
+          .getInstance(OnStaffModeDisabled.class)
+          .create(WebHookClient.fromURL(config.get().staffDisabled.url())), addonContainer
+      );
+    }
+
+    if (config.get().staffEnabled.enabled()) {
+      addonManager.registerListener(addonInjector
+          .getInstance(OnStaffModeEnabled.class)
+          .create(WebHookClient.fromURL(config.get().staffEnabled.url())), addonContainer
+      );
+    }
+
+    if (config.get().staffJoin.enabled()) {
+      addonManager.registerListener(addonInjector
+          .getInstance(OnStaffJoin.class)
+          .create(WebHookClient.fromURL(config.get().staffJoin.url())), addonContainer
+      );
+    }
+
+    if (config.get().staffLeft.enabled()) {
+      addonManager.registerListener(addonInjector
+          .getInstance(OnStaffLeave.class)
+          .create(WebHookClient.fromURL(config.get().staffLeft.url())), addonContainer
       );
     }
   }
