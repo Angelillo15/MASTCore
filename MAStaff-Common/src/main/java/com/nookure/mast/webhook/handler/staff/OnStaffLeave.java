e1c5f273df85230c159730e8972a0567ebea9e01
Angelillo15 <contact@angelillo15.es>
2023-10-27 19:12:45 +0200

    Refactor OnStaffLeave

--- MAStaff-Common/src/main/java/com/nookure/mast/webhook/handler/staff/OnStaffLeave.java
+++ MAStaff-Common/src/main/java/com/nookure/mast/webhook/handler/staff/OnStaffLeave.java
@@ -1,4 +1,4 @@
-package com.nookure.mast.webhook.handler;
+package com.nookure.mast.webhook.handler.staff;
 
 import com.google.inject.Inject;
 import com.nookure.mast.api.event.MastSubscribe;
@@ -9,7 +9,7 @@ import com.nookure.mast.webhook.Webhooks;
 import com.nookure.mast.webhook.json.WebhookLoader;
 import es.angelillo15.mast.api.factory.CommonFactory;
 
-public class StaffLeave implements CommonFactory<StaffLeave, WebHookClient> {
+public class OnStaffLeave implements CommonFactory<OnStaffLeave, WebHookClient> {
   @Inject
   private WebhookLoader loader;
   private WebHookClient client;
@@ -23,7 +23,7 @@ public class StaffLeave implements CommonFactory<StaffLeave, WebHookClient> {
   }
 
   @Override
-  public StaffLeave create(WebHookClient factory) {
+  public OnStaffLeave create(WebHookClient factory) {
     client = factory;
     return this;
   }
