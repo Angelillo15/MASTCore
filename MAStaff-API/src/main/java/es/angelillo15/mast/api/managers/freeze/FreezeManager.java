5aae29a0baf907597c0b8d9cdfea1403e52ba58f
Angelillo15 <contact@angelillo15.es>
2023-11-07 21:36:50 +0100

    Delete old FreezeManager and create a new one using Dependency Injection

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/managers/freeze/FreezeManager.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/managers/freeze/FreezeManager.java
@@ -1,68 +1,81 @@
 package es.angelillo15.mast.api.managers.freeze;
 
+import com.google.inject.Singleton;
 import es.angelillo15.mast.api.IStaffPlayer;
-import java.util.ArrayList;
-import java.util.HashMap;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Singleton
 public class FreezeManager {
-    public static HashMap<String, FreezeVector> frozenPlayers = new HashMap<>();
+  public HashMap<String, FreezeVector> frozenPlayers = new HashMap<>();
 
-    /**
-     * Freeze the player
-     * @param staff IStaffPlayer who freeze the player
-     * @param target Player to freeze
-     */
-    public static void freezePlayer(IStaffPlayer staff, Player target) {
-        frozenPlayers.put(target.getName(), new FreezeVector(staff, target));
-    }
+  /**
+   * Freeze the player
+   * @param staff IStaffPlayer who freeze the player
+   * @param target Player to freeze
+   */
+  public void freezePlayer(IStaffPlayer staff, Player target) {
+    freezePlayer(staff, target, -1);
+  }
 
-    /**
-     * Unfreeze the player
-     * @param target Player
-     */
-    public static void unfreezePlayer(Player target) {
-        frozenPlayers.remove(target.getName());
-    }
+  /**
+   * Freeze the player
+   * @param staff IStaffPlayer who freeze the player
+   * @param target Player to freeze
+   * @param time Time to freeze the player (in ms)
+   */
+  public void freezePlayer(IStaffPlayer staff, Player target, long time) {
+    frozenPlayers.put(target.getName(), new FreezeVector(staff, target, System.currentTimeMillis() + time));
+  }
 
-    /**
-     * Unfreeze the player
-     * @param target Player
-     */
-    public static void unfreezePlayer(String target) {
-        frozenPlayers.remove(target);
-    }
+  /**
+   * Unfreeze the player
+   * @param target Player
+   */
+  public void unfreezePlayer(Player target) {
+    frozenPlayers.remove(target.getName());
+  }
 
-    /**
-     * Check if the player is frozen
-     * @param target Player
-     * @return boolean
-     */
-    public static boolean isFrozen(Player target) {
-        return frozenPlayers.containsKey(target.getName());
-    }
+  /**
+   * Unfreeze the player
+   * @param target Player
+   */
+  public void unfreezePlayer(String target) {
+    frozenPlayers.remove(target);
+  }
 
-    /**
-     * Check if the player is frozen
-     * @param target Player
-     * @return boolean
-     */
-    public static boolean isFrozen(String target) {
-        return frozenPlayers.containsKey(target);
-    }
+  /**
+   * Check if the player is frozen
+   * @param target Player
+   * @return boolean
+   */
+  public boolean isFrozen(Player target) {
+    return frozenPlayers.containsKey(target.getName());
+  }
 
-    /**
-     * Gets the frozen players list
-     * @return ArrayList of OfflinePlayer
-     */
-    public static ArrayList<OfflinePlayer> getFrozenPlayers() {
-        ArrayList<OfflinePlayer> frozen = new ArrayList<>();
-        frozenPlayers.forEach((name, vector) -> frozen.add(vector.getTarget()));
-        return frozen;
-    }
+  /**
+   * Check if the player is frozen
+   * @param target Player
+   * @return boolean
+   */
+  public boolean isFrozen(String target) {
+    return frozenPlayers.containsKey(target);
+  }
 
-    public static FreezeVector getFreezeVector(Player target) {
-        return frozenPlayers.get(target.getName());
-    }
+  /**
+   * Gets the frozen players list
+   * @return ArrayList of OfflinePlayer
+   */
+  public ArrayList<OfflinePlayer> getFrozenPlayers() {
+    ArrayList<OfflinePlayer> frozen = new ArrayList<>();
+    frozenPlayers.forEach((name, vector) -> frozen.add(vector.getTarget()));
+    return frozen;
+  }
+
+  public FreezeVector getFreezeVector(Player target) {
+    return frozenPlayers.get(target.getName());
+  }
 }
