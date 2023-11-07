16c8a678494a6feb9032e70163a90dc1d8dae0af
Angelillo15 <contact@angelillo15.es>
2023-11-07 21:37:04 +0100

    Delete lombok logic

--- MAStaff-API/src/main/java/es/angelillo15/mast/api/managers/freeze/FreezeVector.java
+++ MAStaff-API/src/main/java/es/angelillo15/mast/api/managers/freeze/FreezeVector.java
@@ -1,13 +1,41 @@
 package es.angelillo15.mast.api.managers.freeze;
 
 import es.angelillo15.mast.api.IStaffPlayer;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.bukkit.OfflinePlayer;
 
-@Data
-@AllArgsConstructor
+
 public class FreezeVector {
   IStaffPlayer staffPlayer;
   OfflinePlayer target;
+  long timeLeft;
+
+  public FreezeVector(IStaffPlayer staffPlayer, OfflinePlayer target, long timeLeft) {
+    this.staffPlayer = staffPlayer;
+    this.target = target;
+    this.timeLeft = timeLeft;
+  }
+
+  public IStaffPlayer getStaffPlayer() {
+    return staffPlayer;
+  }
+
+  public void setStaffPlayer(IStaffPlayer staffPlayer) {
+    this.staffPlayer = staffPlayer;
+  }
+
+  public OfflinePlayer getTarget() {
+    return target;
+  }
+
+  public void setTarget(OfflinePlayer target) {
+    this.target = target;
+  }
+
+  public long getTimeLeft() {
+    return timeLeft;
+  }
+
+  public void setTimeLeft(int timeLeft) {
+    this.timeLeft = timeLeft;
+  }
 }
