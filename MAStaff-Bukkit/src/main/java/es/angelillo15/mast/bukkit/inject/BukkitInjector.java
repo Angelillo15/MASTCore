31d8a5eb111f3a01514e5fc6373e87461c691d73
Angelillo15 <contact@angelillo15.es>
2023-11-08 21:14:58 +0100

    Bind FreezeManager to the injector

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/inject/BukkitInjector.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/inject/BukkitInjector.java
@@ -2,6 +2,7 @@ package es.angelillo15.mast.bukkit.inject;
 
 import com.nookure.mast.addon.ServerAddonManager;
 import com.nookure.mast.api.addons.AddonManager;
+import com.nookure.mast.api.manager.FreezeManager;
 import com.nookure.mast.api.staff.StaffFeatureManager;
 import es.angelillo15.mast.api.IServerUtils;
 import com.nookure.mast.api.manager.cmd.CommandBukkitSenderManager;
@@ -31,5 +32,6 @@ public class BukkitInjector extends CommonModule {
     bind(CommandBukkitSenderManager.class).asEagerSingleton();
     bind(AddonManager.class).to(ServerAddonManager.class).asEagerSingleton();
     bind(StaffFeatureManager.class).to(BukkitStaffFeatureManager.class).asEagerSingleton();
+    bind(FreezeManager.class).asEagerSingleton();
   }
 }
