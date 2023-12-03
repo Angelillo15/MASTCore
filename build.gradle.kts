39a9259e994ee410dcb568195e6f92350b106660
Angelillo15 <contact@angelillo15.es>
2023-12-03 16:49:05 +0100

    Delete nookure repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,6 +72,7 @@ allprojects {
 
   repositories {
     mavenCentral()
+    mavenLocal()
     maven("https://jitpack.io")
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots")
@@ -83,7 +84,6 @@ allprojects {
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     maven("https://mvn.exceptionflug.de/repository/exceptionflug-public/")
     maven("https://repo.simplix.dev/repository/simplixsoft-public")
-    maven("https://repo.nookure.com/releases")
     maven("https://repo.codemc.io/repository/nms/")
   }
 
