a3335d427f5098292f776e7de49fab93f4f1c196
Angelillo15 <contact@angelillo15.es>
2023-12-15 16:59:42 +0100

    Add new bindings

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/inject/BukkitInjector.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/inject/BukkitInjector.java
@@ -2,6 +2,7 @@ package es.angelillo15.mast.bukkit.inject;
 
 import com.nookure.mast.addon.ServerAddonManager;
 import com.nookure.mast.api.addons.AddonManager;
+import com.nookure.mast.api.event.PluginMessageManager;
 import com.nookure.mast.api.manager.FreezeManager;
 import com.nookure.mast.api.staff.StaffFeatureManager;
 import es.angelillo15.mast.api.IServerUtils;
@@ -11,6 +12,7 @@ import es.angelillo15.mast.api.MAStaffInstance;
 import es.angelillo15.mast.api.inject.CommonModule;
 import es.angelillo15.mast.api.managers.StaffManager;
 import es.angelillo15.mast.api.utils.MAStaffInject;
+import es.angelillo15.mast.bukkit.BukkitPluginMessageManager;
 import es.angelillo15.mast.bukkit.MAStaff;
 import es.angelillo15.mast.bukkit.ServerUtils;
 import es.angelillo15.mast.bukkit.manager.BukkitStaffFeatureManager;
@@ -33,5 +35,6 @@ public class BukkitInjector extends CommonModule {
     bind(AddonManager.class).to(ServerAddonManager.class).asEagerSingleton();
     bind(StaffFeatureManager.class).to(BukkitStaffFeatureManager.class).asEagerSingleton();
     bind(FreezeManager.class).asEagerSingleton();
+    bind(PluginMessageManager.class).to(BukkitPluginMessageManager.class).asEagerSingleton();
   }
 }
