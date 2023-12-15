52c6125df49cca37703a38ac490bbf310c743dee
Angelillo15 <contact@angelillo15.es>
2023-12-15 21:05:17 +0100

    Change StaffModeEnabledEvent from class to record

--- MAStaff-API/src/main/java/com/nookure/mast/api/event/staff/mode/StaffModeEnabledEvent.java
+++ MAStaff-API/src/main/java/com/nookure/mast/api/event/staff/mode/StaffModeEnabledEvent.java
@@ -3,14 +3,5 @@ package com.nookure.mast.api.event.staff.mode;
 import com.nookure.mast.api.event.Event;
 import es.angelillo15.mast.api.IStaffPlayer;
 
-public class StaffModeEnabledEvent extends Event {
-  private final IStaffPlayer staffPlayer;
-
-  public StaffModeEnabledEvent(IStaffPlayer staffPlayer) {
-    this.staffPlayer = staffPlayer;
-  }
-
-  public IStaffPlayer getStaffPlayer() {
-    return staffPlayer;
-  }
+public record StaffModeEnabledEvent(IStaffPlayer staffPlayer) implements Event {
 }
