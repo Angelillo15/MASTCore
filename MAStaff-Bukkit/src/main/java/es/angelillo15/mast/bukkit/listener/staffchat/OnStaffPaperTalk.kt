0acdc534ea4a055236686731c6ae025d1e63e9fb
Angelillo15 <contact@angelillo15.es>
2023-12-15 21:09:45 +0100

    Change to new logic of PM

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/listener/staffchat/OnStaffPaperTalk.kt
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/listener/staffchat/OnStaffPaperTalk.kt
@@ -1,20 +1,27 @@
 package es.angelillo15.mast.bukkit.listener.staffchat
 
-import es.angelillo15.mast.handler.OnStaffMessageEvent
+import com.google.inject.Inject
+import com.nookure.mast.api.cmd.sender.PlayerCommandSender
+import com.nookure.mast.api.manager.cmd.CommandBukkitSenderManager
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 
 class OnStaffPaperTalk : OnStaffMessageEvent(), Listener {
-  @EventHandler
+  @Inject
+  private lateinit var commandManager: CommandBukkitSenderManager
+  @EventHandler(
+      ignoreCancelled = true,
+      priority = EventPriority.HIGH
+  )
   fun onPlayerTalk(event: AsyncChatEvent) {
     if (!event.player.hasPermission("mast.staffchat")) return
 
-    if (onStaffMessageEvent(
-            event.player.name, MiniMessage.miniMessage().serialize(event.message()), "Paper"
-          )
-    ) {
+    if (onStaffMessageEvent(commandManager.getSender(event.player) as PlayerCommandSender,
+            MiniMessage.miniMessage().serialize(event.message())
+    )) {
       event.isCancelled = true
     }
   }
