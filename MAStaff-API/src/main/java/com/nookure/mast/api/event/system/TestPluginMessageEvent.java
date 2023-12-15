20f7b58d7fcf7d1c2cf99795b6e9dee175c0a4ab
Angelillo15 <contact@angelillo15.es>
2023-12-15 17:01:06 +0100

    Create test event

---
+++ MAStaff-API/src/main/java/com/nookure/mast/api/event/system/TestPluginMessageEvent.java
@@ -0,0 +1,10 @@
+package com.nookure.mast.api.event.system;
+
+import com.nookure.mast.api.event.Event;
+
+/**
+ * This event is used to test the plugin message system
+ * It is not used in the plugin
+ */
+public class TestPluginMessageEvent extends Event {
+}
