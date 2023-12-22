1875f56f4ab7c95b83bdd48102950f7bae5954e2
Angelillo15 <contact@angelillo15.es>
2023-12-22 15:05:48 +0100

    Addapt color utils

--- MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/utils/ColorUtils.java
+++ MAStaff-Bukkit/src/main/java/es/angelillo15/mast/bukkit/utils/ColorUtils.java
@@ -1,7 +1,9 @@
 package es.angelillo15.mast.bukkit.utils;
 
+import es.angelillo15.mast.api.Replace;
 import es.angelillo15.mast.api.TextUtils;
 import es.angelillo15.mast.api.config.bukkit.ConfigLoader;
+import es.angelillo15.mast.api.config.bukkit.Messages;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
@@ -13,11 +15,25 @@ public class ColorUtils {
   }
 
   public static String raw(String path) {
-    return ConfigLoader.getMessages().getConfig().getString(path);
+    if (getMessages().contains(path)) {
+      return getMessages().getString(path).replace("{prefix}", Messages.PREFIX());
+    } else {
+      return path;
+    }
   }
 
   public static Component cmp(String path) {
     return MiniMessage.miniMessage().deserialize(tl(path));
+  }
+
+  public static Component cmp(String path, Replace... replace) {
+    String text = tl(path);
+    for (Replace r : replace) {
+      text = text.replace( "{" + r.search() + "}", r.replace());
+    }
+
+    text = text.replace("{prefix}", Messages.PREFIX());
+    return MiniMessage.miniMessage().deserialize(text);
   }
 
   public static String chc(String path) {
