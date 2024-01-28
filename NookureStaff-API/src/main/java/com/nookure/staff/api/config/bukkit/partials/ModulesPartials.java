bd559552328a50f7ef676403b3bf5a20784f1a83
Angelillo15 <contact@angelillo15.es>
2024-01-28 18:11:58 +0100

    Create module partials

---
+++ NookureStaff-API/src/main/java/com/nookure/staff/api/config/bukkit/partials/ModulesPartials.java
@@ -0,0 +1,66 @@
+package com.nookure.staff.api.config.bukkit.partials;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
+import org.spongepowered.configurate.objectmapping.meta.Setting;
+
+@ConfigSerializable
+public class ModulesPartials {
+  @Setting
+  @Comment("""
+      Enable or disable the staff mode.
+      This include /staff command and the staff
+      vault.
+      """)
+  private boolean staffMode = true;
+
+  @Setting
+  @Comment("""
+      Enable or disable the freeze module.
+      This include /freeze command
+      """)
+  private boolean freeze = true;
+
+  @Setting
+  @Comment("""
+      Enable or disable the vanish module.
+      This include /vanish command and the plugin vanish
+      on StaffMode.
+      """)
+  private boolean vanish = true;
+
+  @Setting
+  @Comment("""
+      Enable or disable the invsee module.
+      This include /invsee command
+      """)
+  private boolean invsee = true;
+
+  @Setting
+  @Comment("""
+      Enable or disable the StaffChat module.
+      This include /staffchat command and 
+      the staff chat prefix.
+      """)
+  private boolean staffChat = true;
+
+  public boolean isStaffMode() {
+    return staffMode;
+  }
+
+  public boolean isFreeze() {
+    return freeze;
+  }
+
+  public boolean isVanish() {
+    return vanish;
+  }
+
+  public boolean isInvsee() {
+    return invsee;
+  }
+
+  public boolean isStaffChat() {
+    return staffChat;
+  }
+}
