7092be4352f8240210ce3e74cb34a1cc10d1585a
Angelillo15 <contact@angelillo15.es>
2024-01-28 18:07:15 +0100

    Create permissions class

---
+++ NookureStaff-API/src/main/java/com/nookure/staff/api/Permissions.java
@@ -0,0 +1,16 @@
+package com.nookure.staff.api;
+
+/**
+ * Contains all the permissions used by the plugin.
+ *
+ * @since 1.0.0
+ */
+public class Permissions {
+  /**
+   * General permission for staff members.
+   * This permission is required to use any staff command.
+   * With this permission you also will appear as a {@link StaffPlayerWrapper}
+   * instead of a {@link PlayerWrapper}.
+   */
+  public static final String STAFF_PERMISSION = "nookure.staff";
+}
