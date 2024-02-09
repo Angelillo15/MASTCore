eee165cfde7e94ee28de68d06eb880e98bb90c89
Angelillo15 <contact@angelillo15.es>
2024-02-09 21:19:43 +0100

    Create new events for managing states between join & leave

---
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/handler/OnPlayerLeave.java
@@ -0,0 +1,27 @@
+package com.nookure.staff.paper.handler;
+
+import com.google.inject.Inject;
+import com.nookure.staff.api.Logger;
+import com.nookure.staff.api.manager.PlayerWrapperManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class OnPlayerLeave implements Listener {
+  @Inject
+  private PlayerWrapperManager<Player> playerWrapperManager;
+  @Inject
+  private Logger logger;
+
+  @EventHandler(
+      priority = EventPriority.HIGHEST
+  )
+  public void onPlayerLeave(PlayerQuitEvent event) {
+    logger.debug("Player " + event.getPlayer().getName() + " has left the server");
+    logger.debug("Removing player wrapper for " + event.getPlayer().getName());
+
+    playerWrapperManager.removePlayerWrapper(event.getPlayer());
+  }
+}
