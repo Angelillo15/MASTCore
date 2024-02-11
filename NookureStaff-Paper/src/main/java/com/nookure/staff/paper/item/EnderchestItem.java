96393dcbf623c51aa81c2bb525f3be8df8e5c2fe
Angelillo15 <contact@angelillo15.es>
2024-02-11 01:55:07 +0100

    Filled methods from enderchest item and InventorySeeItem

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/item/EnderchestItem.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/item/EnderchestItem.java
@@ -7,6 +7,8 @@ import com.nookure.staff.api.config.bukkit.ItemsConfig;
 import com.nookure.staff.api.item.Items;
 import com.nookure.staff.api.item.PlayerInteractItem;
 import com.nookure.staff.api.item.StaffItem;
+import com.nookure.staff.paper.PaperPlayerWrapper;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class EnderchestItem extends StaffItem implements PlayerInteractItem {
@@ -17,6 +19,9 @@ public class EnderchestItem extends StaffItem implements PlayerInteractItem {
 
   @Override
   public void click(@NotNull PlayerWrapper player, @NotNull PlayerWrapper target) {
+    Player bukkitPlayer = ((PaperPlayerWrapper) player).getPlayer();
+    Player bukkitTarget = ((PaperPlayerWrapper) target).getPlayer();
 
+    bukkitPlayer.openInventory(bukkitTarget.getEnderChest());
   }
 }
