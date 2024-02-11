8822d6be3e52e84c3a59552b713059963e22c34c
Angelillo15 <contact@angelillo15.es>
2024-02-11 16:04:54 +0100

    Add new messages

--- NookureStaff-API/src/main/java/com/nookure/staff/api/config/bukkit/BukkitMessages.java
+++ NookureStaff-API/src/main/java/com/nookure/staff/api/config/bukkit/BukkitMessages.java
@@ -1,9 +1,25 @@
 package com.nookure.staff.api.config.bukkit;
 
 import com.nookure.staff.api.config.bukkit.partials.messages.StaffModePartial;
+import com.nookure.staff.api.config.bukkit.partials.messages.VanishPartial;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
+import org.spongepowered.configurate.objectmapping.meta.Setting;
 
 @ConfigSerializable
 public class BukkitMessages {
+  @Setting
+  @Comment(
+      """
+          The prefix for all staff messages.
+          If you want to use the prefix in a message, use {prefix}.
+              """
+  )
+  private String prefix = "<b><red>Staff</red> <gray>»</gray></b>";
   public final StaffModePartial staffMode = new StaffModePartial();
+  public final VanishPartial vanish = new VanishPartial();
+
+  public String prefix() {
+    return prefix;
+  }
 }
