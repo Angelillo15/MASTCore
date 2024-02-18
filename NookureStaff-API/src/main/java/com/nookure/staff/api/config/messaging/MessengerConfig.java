1381d3c5d63d9de7350d2f5034e755c095e7b743
Angelillo15 <contact@angelillo15.es>
2024-02-18 12:32:49 +0100

    Create MessengerConfig

--- NookureStaff-API/src/main/java/com/nookure/staff/api/config/messaging/MessengerConfig.java
+++ NookureStaff-API/src/main/java/com/nookure/staff/api/config/messaging/MessengerConfig.java
@@ -1,9 +1,41 @@
 package com.nookure.staff.api.config.messaging;
 
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
+import org.spongepowered.configurate.objectmapping.meta.Setting;
+
+@ConfigSerializable
 public class MessengerConfig {
-  enum MessengerType {
+  public enum MessengerType {
     REDIS,
-    SQL,
-    PM
+    PM,
+    NONE
+  }
+
+  @Setting
+  @Comment(
+      """
+          The type of messenger to use.
+          Here are the options:
+          - REDIS: Use Redis to send messages between servers (recommended)
+          - PM: Use plugin messages to send messages between servers this
+          will only work if all the servers are under the same proxy and
+          in the proxy is installed the bridge plugin.
+          - NONE: Disable the sync, useful when you don't have a network
+          and you only have 1 server.
+              """
+  )
+  private MessengerType type = MessengerType.PM;
+
+  @Setting
+  @Comment(
+      """
+          The configuration for the Redis messenger.
+              """
+  )
+  public final RedisPartial redis = new RedisPartial();
+
+  public MessengerType getType() {
+    return type;
   }
 }
