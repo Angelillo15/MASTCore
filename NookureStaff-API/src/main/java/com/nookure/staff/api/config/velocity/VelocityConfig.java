db5d2ba9a9daa380ba469ce33232e5b5d601b2fe
Angelillo15 <contact@angelillo15.es>
2024-02-18 12:29:12 +0100

    Create VelocityModule classes

---
+++ NookureStaff-API/src/main/java/com/nookure/staff/api/config/velocity/VelocityConfig.java
@@ -0,0 +1,20 @@
+package com.nookure.staff.api.config.velocity;
+
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
+import org.spongepowered.configurate.objectmapping.meta.Setting;
+
+@ConfigSerializable
+public class VelocityConfig {
+  @Setting
+  @Comment("""
+      Enable or disable debug mode.
+      This will print out more information to the console.
+      It's recommended to find bugs.
+      """)
+  private boolean debug = false;
+
+  public boolean isDebug() {
+    return debug;
+  }
+}
---
+++ NookureStaff-Velocity/src/main/java/com/nookure/staff/velocity/module/VelocityPluginModule.java
@@ -0,0 +1,43 @@
+package com.nookure.staff.velocity.module;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+import com.nookure.staff.api.Logger;
+import com.nookure.staff.api.NookureStaffPlatform;
+import com.nookure.staff.api.config.ConfigurationContainer;
+import com.nookure.staff.api.config.velocity.VelocityConfig;
+import com.nookure.staff.velocity.NookureStaff;
+import com.velocitypowered.api.proxy.ProxyServer;
+
+import java.io.IOException;
+
+public class VelocityPluginModule extends AbstractModule {
+  private final NookureStaff plugin;
+
+  public VelocityPluginModule(NookureStaff plugin) {
+    this.plugin = plugin;
+  }
+
+  @Override
+  protected void configure() {
+    bind(Logger.class).toInstance(plugin.getPLogger());
+    bind(com.nookure.staff.api.NookureStaff.class).toInstance(plugin);
+
+    bind(new TypeLiteral<NookureStaffPlatform<ProxyServer>>() {
+    }).toInstance(plugin);
+
+    try {
+      bind(new TypeLiteral<ConfigurationContainer<VelocityConfig>>() {
+      }).toInstance(loadVelocity());
+    } catch (IOException e) {
+      plugin.getPLogger().severe("An error occurred while creating config files, %s", e.getMessage());
+      throw new RuntimeException(e);
+    }
+  }
+
+  private ConfigurationContainer<VelocityConfig> loadVelocity() throws IOException {
+    ConfigurationContainer<VelocityConfig> config = ConfigurationContainer.load(plugin.getPluginDataFolder().toPath(), VelocityConfig.class);
+    plugin.setDebug(config.get().isDebug());
+    return config;
+  }
+}
