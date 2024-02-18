5be847e460ef13b469485803189501f06dfc46d3
Angelillo15 <contact@angelillo15.es>
2024-02-18 12:30:23 +0100

    Add velocity as an annotation processor

--- NookureStaff-Velocity/build.gradle.kts
+++ NookureStaff-Velocity/build.gradle.kts
@@ -1,19 +1,5 @@
-plugins {
-    id("java")
-}
-
-group = "com.nookure.staff"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation(project(":NookureStaff-API"))
+    compileOnly(libs.velocity)
+    annotationProcessor(libs.velocity)
 }
