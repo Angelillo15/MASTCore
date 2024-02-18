961c3ef6a689f93218b0d4e3b1a9b7fb5eb3563f
Angelillo15 <contact@angelillo15.es>
2024-02-18 12:32:01 +0100

    Improve the publishing and decoding logic

--- NookureStaff-API/src/main/java/com/nookure/staff/api/messaging/EventMessenger.java
+++ NookureStaff-API/src/main/java/com/nookure/staff/api/messaging/EventMessenger.java
@@ -7,8 +7,7 @@ import com.nookure.staff.api.PlayerWrapper;
 import com.nookure.staff.api.event.Event;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -29,15 +28,36 @@ public abstract class EventMessenger implements AutoCloseable {
    * @param sender The sender of the event
    * @param data   The event data
    */
-  public abstract void publish(@NotNull PlayerWrapper sender, byte @NotNull[] data);
+  public abstract void publish(@NotNull PlayerWrapper sender, byte @NotNull [] data);
 
-  @NotNull
-  public Optional<Event> decodeEvent(@NotNull ObjectInputStream objetStream) {
-    Objects.requireNonNull(objetStream);
+  public void publish(@NotNull PlayerWrapper sender, @NotNull Event event) {
+    Objects.requireNonNull(sender);
+    Objects.requireNonNull(event);
 
     try {
-      return Optional.of((Event) objetStream.readObject());
-    } catch (IOException | ClassNotFoundException e) {
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+      ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+      objectOutputStream.writeObject(event);
+      objectOutputStream.flush();
+
+
+      publish(sender, byteArrayOutputStream.toByteArray());
+    } catch (IOException e) {
+      logger.severe("Error while serializing event");
+      if (plugin.isDebug()) {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+  @NotNull
+  public Optional<Event> decodeEvent(byte @NotNull [] message) {
+    Objects.requireNonNull(message);
+
+    try {
+      ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(message));
+      return Optional.of((Event) objectInputStream.readObject());
+    } catch (Exception e) {
       logger.severe("Error while decoding event from object stream");
       if (plugin.isDebug()) {
         throw new RuntimeException(e);
