91505f82ce5f03666f96804413fcbef283952b88
Angelillo15 <contact@angelillo15.es>
2024-02-20 23:05:54 +0100

    Impl double jedis instance for publishing and subscribing

--- NookureStaff-Common/src/main/java/com/nookure/staff/messaging/RedisMessenger.java
+++ NookureStaff-Common/src/main/java/com/nookure/staff/messaging/RedisMessenger.java
@@ -5,38 +5,56 @@ import com.google.inject.Injector;
 import com.google.inject.Singleton;
 import com.nookure.staff.api.Logger;
 import com.nookure.staff.api.PlayerWrapper;
+import com.nookure.staff.api.annotation.RedisPublish;
+import com.nookure.staff.api.annotation.RedisSubscribe;
 import com.nookure.staff.api.event.EventManager;
 import com.nookure.staff.api.messaging.EventMessenger;
+import com.nookure.staff.api.util.Scheduler;
 import org.jetbrains.annotations.NotNull;
 import redis.clients.jedis.BinaryJedisPubSub;
 import redis.clients.jedis.Jedis;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 @Singleton
 public class RedisMessenger extends EventMessenger {
   @Inject
-  private Jedis jedis;
+  @RedisPublish
+  private Jedis jedisPublish;
+  @Inject
+  @RedisSubscribe
+  private Jedis jedisSubscribe;
   @Inject
   private Injector injector;
+  @Inject
+  private Logger logger;
+  @Inject
+  private Scheduler scheduler;
+  private BinaryJedisPubSub messenger;
+  private static final byte[] CHANNEL = "nkstaff_events".getBytes();
 
-  private static final byte[] CHANNEL = "nookure:staff:events".getBytes(StandardCharsets.UTF_8);
+  private int subscribeTaskID;
 
   @Override
   public void prepare() {
-    if (!jedis.isConnected()) {
-      jedis.connect();
+    if (!jedisPublish.isConnected()) {
+      logger.debug("Connecting to redis publish...");
+      jedisPublish.connect();
     }
 
-    jedis.subscribe(injector.getInstance(RedisEventMessenger.class), CHANNEL);
+    if (!jedisSubscribe.isConnected()) {
+      logger.debug("Connecting to redis subscribe...");
+      jedisSubscribe.connect();
+    }
+
+    logger.debug("Subscribing to redis channel " + Arrays.toString(CHANNEL));
+    messenger = injector.getInstance(RedisEventMessenger.class);
+    subscribeTaskID = scheduler.async(() -> jedisSubscribe.subscribe(messenger, CHANNEL));
   }
 
   @Override
   public void publish(@NotNull PlayerWrapper sender, byte @NotNull [] data) {
-    jedis.publish(CHANNEL, data);
+    scheduler.async(() -> jedisPublish.publish(CHANNEL, data));
   }
 
   private static class RedisEventMessenger extends BinaryJedisPubSub {
@@ -48,15 +66,35 @@ public class RedisMessenger extends EventMessenger {
     private Logger logger;
 
     @Override
+    public void onSubscribe(byte[] channel, int subscribedChannels) {
+      logger.debug("Subscribed to redis channel " + Arrays.toString(channel));
+    }
+
+    @Override
+    public void onUnsubscribe(byte[] channel, int subscribedChannels) {
+      logger.debug("Unsubscribed from redis channel " + Arrays.toString(channel));
+    }
+
+    @Override
     public void onMessage(byte[] channel, byte[] message) {
       try {
+        logger.debug("Received message from redis");
         eventTransport.decodeEvent(message).ifPresent(event -> {
-          logger.debug("Received event " + event.getClass().getSimpleName() + " from plugin message");
+          logger.debug("Received event " + event.getClass().getSimpleName() + " from redis");
           eventManager.fireEvent(event);
         });
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
     }
+  }
+
+  @Override
+  public void close() throws Exception {
+    messenger.unsubscribe();
+    scheduler.cancel(subscribeTaskID);
+
+    jedisSubscribe.close();
+    jedisPublish.close();
   }
 }
