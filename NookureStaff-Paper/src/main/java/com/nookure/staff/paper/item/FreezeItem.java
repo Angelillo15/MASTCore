77433b54cd9f3a833aa7362d1ea0b9ab61d5a2c4
Angelillo15 <contact@angelillo15.es>
2024-03-10 19:53:52 +0100

    Add freeze methods to the item

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/item/FreezeItem.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/item/FreezeItem.java
@@ -2,21 +2,36 @@ package com.nookure.staff.paper.item;
 
 import com.google.inject.Inject;
 import com.nookure.staff.api.PlayerWrapper;
+import com.nookure.staff.api.StaffPlayerWrapper;
 import com.nookure.staff.api.config.ConfigurationContainer;
 import com.nookure.staff.api.config.bukkit.ItemsConfig;
 import com.nookure.staff.api.item.Items;
 import com.nookure.staff.api.item.PlayerInteractItem;
 import com.nookure.staff.api.item.StaffItem;
+import com.nookure.staff.api.manager.FreezeManager;
+import com.nookure.staff.paper.extension.FreezePlayerExtension;
 import org.jetbrains.annotations.NotNull;
 
 public class FreezeItem extends StaffItem implements PlayerInteractItem {
+  private final FreezeManager freezeManager;
+
   @Inject
-  public FreezeItem(ConfigurationContainer<ItemsConfig> itemsConfig) {
+  public FreezeItem(ConfigurationContainer<ItemsConfig> itemsConfig, FreezeManager freezeManager) {
     super(itemsConfig.get().staffItems.getItems().get(Items.FREEZE.toString()));
+    this.freezeManager = freezeManager;
   }
 
   @Override
   public void click(@NotNull PlayerWrapper player, @NotNull PlayerWrapper target) {
+    if (player instanceof StaffPlayerWrapper staff) {
+      staff.getExtension(FreezePlayerExtension.class).ifPresent(freezePlayerExtension -> {
+        if (freezeManager.isFrozen(target)) {
+          freezePlayerExtension.unfreezePlayer(target);
+          return;
+        }
 
+        freezePlayerExtension.freezePlayer(target);
+      });
+    }
   }
 }
