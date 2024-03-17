b093544a4fdc09a83e27853b1fb0953e52e32075
Angelillo15 <contact@angelillo15.es>
2024-03-17 19:12:22 +0100

    Create reload subcommand

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/command/main/ReloadSubCommand.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/command/main/ReloadSubCommand.java
@@ -1,8 +1,12 @@
 package com.nookure.staff.paper.command.main;
 
+import com.google.inject.Inject;
+import com.nookure.staff.api.NookureStaff;
 import com.nookure.staff.api.command.Command;
 import com.nookure.staff.api.command.CommandData;
 import com.nookure.staff.api.command.CommandSender;
+import com.nookure.staff.api.config.ConfigurationContainer;
+import com.nookure.staff.api.config.bukkit.BukkitMessages;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -12,8 +16,14 @@ import java.util.List;
     description = "Reloads the plugin"
 )
 public class ReloadSubCommand extends Command {
+  @Inject
+  private NookureStaff nookureStaff;
+  @Inject
+  private ConfigurationContainer<BukkitMessages> messages;
+
   @Override
   public void onCommand(@NotNull CommandSender sender, @NotNull String label, @NotNull List<String> args) {
-
+    nookureStaff.reload();
+    sender.sendMiniMessage(messages.get().reload());
   }
 }
