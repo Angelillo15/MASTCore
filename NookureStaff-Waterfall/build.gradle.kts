948eff47a8e9770862b38b6a71c127f70747a12e
Angelillo15 <contact@angelillo15.es>
2024-03-19 17:24:10 +0100

    Create waterfall module build.gradle.kts

--- NookureStaff-Waterfall/build.gradle.kts
+++ NookureStaff-Waterfall/build.gradle.kts
@@ -1,19 +1,23 @@
 plugins {
-    id("java")
-}
-
-group = "com.nookure.staff"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+  alias(libs.plugins.pluginYmlBungee)
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  implementation(project(":NookureStaff-API"))
+  implementation(project(":NookureStaff-Common"))
+  compileOnly(libs.waterfall)
+  implementation(libs.miniMessage)
+  implementation(libs.adventureApi)
+  implementation(libs.textLegacySerializer)
 }
 
-tasks.test {
-    useJUnitPlatform()
+bungee {
+  main = "com.nookure.staff.waterfall.NookureStaff"
+  author = "Angelillo15"
+  version = rootProject.version.toString()
+  description = "A staff plugin bridge by Nookure"
+}
+
+tasks.shadowJar {
+  archiveFileName.set("NookureStaff-Waterfall-${rootProject.version}.jar")
 }
