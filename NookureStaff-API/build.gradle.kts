27f9369d06054c28ff65fecbedcf2c86217edbc8
Angelillo15 <contact@angelillo15.es>
2024-04-30 18:21:53 +0200

    Add publish to api

--- NookureStaff-API/build.gradle.kts
+++ NookureStaff-API/build.gradle.kts
@@ -1,22 +1,94 @@
 plugins {
   id("net.kyori.blossom") version "2.1.0"
   alias(libs.plugins.grgit)
+  alias(libs.plugins.grgitPublish)
+  id("java")
+  id("java-library")
+  id("maven-publish")
+}
+
+java {
+  withJavadocJar()
+  withSourcesJar()
+
+  sourceSets["main"].java {
+    srcDir("src/ap/java")
+  }
 }
 
 dependencies {
   testImplementation(platform("org.junit:junit-bom:5.9.1"))
   testImplementation("org.junit.jupiter:junit-jupiter")
-  compileOnly(libs.paperApi)
-  compileOnly(libs.guava)
-  compileOnly(libs.apacheCommons)
-  compileOnly(libs.configurateYaml)
-  compileOnly(libs.jedis)
-  compileOnly(libs.adventureApi)
-  compileOnly(libs.miniMessage)
+  compileOnlyApi(libs.paperApi)
+  compileOnlyApi(libs.guava)
+  compileOnlyApi(libs.guice)
+  compileOnlyApi(libs.apacheCommons)
+  implementation(libs.configurateYaml)
+  compileOnlyApi(libs.jedis)
+  compileOnlyApi(libs.adventureApi)
+  compileOnlyApi(libs.miniMessage)
+  compileOnlyApi(libs.reflections)
+  compileOnlyApi(rootProject.libs.storm)
+  compileOnlyApi(rootProject.libs.adventureApi)
+  compileOnlyApi(rootProject.libs.miniMessage)
+  compileOnlyApi(rootProject.libs.caffeine)
+  compileOnlyApi(rootProject.libs.liblyBukkit)
+
+  compileOnly(libs.auto.service.annotations)
+  annotationProcessor(libs.auto.service)
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "local"
+      url = uri("${rootProject.rootDir}/maven")
+    }
+  }
+
+  publications {
+    create<MavenPublication>("maven") {
+      groupId = "com.nookure.staff"
+      artifactId = "NookureStaff-API"
+      version = "${rootProject.version}"
+      from(components["java"])
+    }
+  }
+}
+
+gitPublish {
+  repoUri = "https://github.com/Nookure/maven.git"
+  branch = "main"
+  fetchDepth = null
+  commitMessage = "NookureStaff ${rootProject.version}"
+
+  contents {
+    from("${rootProject.rootDir}/maven")
+  }
+
+  preserve {
+    include("**")
+  }
 }
 
 tasks.test {
   useJUnitPlatform()
+}
+
+tasks {
+  withType<Javadoc> {
+    val o = options as StandardJavadocDocletOptions
+    o.encoding = "UTF-8"
+    o.source = "17"
+
+    o.links(
+        "https://guava.dev/releases/${libs.guava.get().version}/api/docs/",
+        "https://google.github.io/guice/api-docs/${libs.guice.get().version}/javadoc/",
+        "https://docs.oracle.com/en/java/javase/17/docs/api/",
+        "https://jd.advntr.dev/api/${libs.adventureApi.get().version}/",
+        "https://javadoc.io/doc/com.github.ben-manes.caffeine/caffeine"
+    )
+  }
 }
 
 sourceSets {
@@ -24,7 +96,7 @@ sourceSets {
     blossom {
       javaSources {
         property("version", rootProject.version.toString())
-        property("branch",  grgit.branch.current().name)
+        property("branch", grgit.branch.current().name)
         property("commit", grgit.head().abbreviatedId)
       }
     }
