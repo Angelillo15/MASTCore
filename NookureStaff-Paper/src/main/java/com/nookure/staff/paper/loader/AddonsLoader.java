824d5a73e3767e9d47f18a9264bd7473636b3886
Angelillo15 <contact@angelillo15.es>
2024-05-01 16:31:19 +0200

    Fix addons loader

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/loader/AddonsLoader.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/loader/AddonsLoader.java
@@ -8,12 +8,6 @@ import com.nookure.staff.api.util.AbstractLoader;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Properties;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 public class AddonsLoader implements AbstractLoader {
   @Inject
@@ -33,58 +27,14 @@ public class AddonsLoader implements AbstractLoader {
       }
     }
 
-    File[] folder = scanFolder.listFiles((file, name) -> {
-      if (file.isDirectory()) {
-        return false;
-      }
-
-      return name.endsWith(".jar");
-    });
-
-    if (folder == null) {
-      return;
-    }
-
-    for (File file : folder) {
-      loadAddon(file);
-    }
-  }
-
-  private void loadAddon(File file) {
-    Properties properties = new Properties();
-
     try {
-      URL[] urls = new URL[]{file.toURI().toURL()};
-
-      try (JarFile jarFile = new JarFile(file)) {
-        JarEntry entry = jarFile.getJarEntry("addon.properties");
-        properties.load(jarFile.getInputStream(entry));
-      } catch (IOException e) {
-        logger.severe("Could not load addon %s", file.getName());
-        logger.severe("An error ocurred while reading the file, please check the file and try again");
-        logger.severe(e);
-        return;
-      }
-
-      Class<?> cls;
-
-      try (URLClassLoader classLoader = new URLClassLoader(urls, getClass().getClassLoader())) {
-        cls = classLoader.loadClass(properties.getProperty("main"));
-      } catch (IOException e) {
-        logger.severe("Could not load the main class of addon %s", file.getName());
-        logger.severe("Please check the file and try again");
-        logger.severe(e);
-        return;
-      }
-
-      manager.enableAddon(cls);
-
-    } catch (MalformedURLException e) {
-      logger.severe("Could not load addon %s", file.getName());
-      logger.severe("An error ocurred while parsing the file URL, please check the file and try again");
+      manager.loadAddonsToClasspath(scanFolder.toPath());
+    } catch (IOException e) {
+      logger.severe("Could not load to the classpath the addons in the addons folder");
+      logger.severe("Please contact us for support");
       logger.severe(e);
-      throw new RuntimeException(e);
-    } catch (ClassNotFoundException e) {
+    } catch (Exception e) {
+      logger.severe("An error ocurred while loading the addons");
       logger.severe(e);
     }
   }
