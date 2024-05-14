b592f87198043458535478136e69a220c7282ebe
Angelillo15 <contact@angelillo15.es>
2024-05-14 15:46:42 +0200

    Add command data

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/note/command/ListNoteCommand.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/note/command/ListNoteCommand.java
@@ -1,11 +1,17 @@
 package com.nookure.staff.paper.note.command;
 
 import com.nookure.staff.api.command.Command;
+import com.nookure.staff.api.command.CommandData;
 import com.nookure.staff.api.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+@CommandData(
+    name = "list",
+    description = "List notes of a user",
+    permission = "nookure.staff.note.list"
+)
 public class ListNoteCommand extends Command {
   @Override
   public void onCommand(@NotNull CommandSender sender, @NotNull String label, @NotNull List<String> args) {
--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/note/command/RemoveNoteCommand.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/note/command/RemoveNoteCommand.java
@@ -1,11 +1,16 @@
 package com.nookure.staff.paper.note.command;
 
 import com.nookure.staff.api.command.Command;
+import com.nookure.staff.api.command.CommandData;
 import com.nookure.staff.api.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+@CommandData(
+    name = "remove",
+    description = "Remove a note from a user"
+)
 public class RemoveNoteCommand extends Command {
   @Override
   public void onCommand(@NotNull CommandSender sender, @NotNull String label, @NotNull List<String> args) {
