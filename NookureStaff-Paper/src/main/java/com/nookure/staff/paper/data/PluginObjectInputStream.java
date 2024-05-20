c114d7bd75963ba2e5502fc4d7c746de5cad8516
Angelillo15 <contact@angelillo15.es>
2024-05-20 19:06:10 +0200

    Create PluginObjectInputStream

---
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/data/PluginObjectInputStream.java
@@ -0,0 +1,34 @@
+package com.nookure.staff.paper.data;
+
+import org.bukkit.util.io.BukkitObjectInputStream;
+
+import java.io.IOException;
+
+public class PluginObjectInputStream extends BukkitObjectInputStream {
+  private final static Class<?> wrapper;
+
+  static {
+    try {
+      wrapper = Class.forName("org.bukkit.util.io.Wrapper");
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  protected PluginObjectInputStream() throws IOException, SecurityException {
+  }
+
+  protected PluginObjectInputStream(java.io.InputStream in) throws IOException {
+    super(in);
+  }
+
+  @Override
+  protected Object resolveObject(Object obj) throws IOException {
+
+    if (wrapper.isInstance(obj)) {
+        return obj;
+    }
+
+    return super.resolveObject(obj);
+  }
+}
