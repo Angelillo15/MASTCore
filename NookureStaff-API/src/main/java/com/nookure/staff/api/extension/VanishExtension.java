2488a552eb76dd2e18d3e0ece25f26583b4e47f0
Angelillo15 <contact@angelillo15.es>
2024-07-05 14:25:15 +0200

    Add VanishExtension javadoc

--- NookureStaff-API/src/main/java/com/nookure/staff/api/extension/VanishExtension.java
+++ NookureStaff-API/src/main/java/com/nookure/staff/api/extension/VanishExtension.java
@@ -2,16 +2,47 @@ package com.nookure.staff.api.extension;
 
 import com.nookure.staff.api.StaffPlayerWrapper;
 
+/**
+ * This class represents an extension for the vanish feature
+ * of the player
+ */
 public abstract class VanishExtension extends StaffPlayerExtension {
+  /**
+   * Create a new extension for the player
+   *
+   * @param player the player
+   */
   public VanishExtension(StaffPlayerWrapper player) {
     super(player);
   }
 
+  /**
+   * Enable vanish for the player
+   *
+   * @param silent if true the server will not send a message
+   *               to the player that this player has left the server
+   */
   public abstract void enableVanish(boolean silent);
 
+  /**
+   * Disable vanish for the player
+   *
+   * @param silent if true the server will not send a message
+   *               to the player that this player has joined the server
+   */
   public abstract void disableVanish(boolean silent);
 
+  /**
+   * Check if the player is vanished
+   *
+   * @return true if the player is vanished
+   */
   public abstract boolean isVanished();
 
+  /**
+   * Set the vanished state of the player
+   *
+   * @param vanished true if the player should be vanished
+   */
   public abstract void setVanished(boolean vanished);
 }
