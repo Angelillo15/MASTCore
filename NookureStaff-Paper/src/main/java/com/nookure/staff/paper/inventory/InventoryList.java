6276056da2a7a0a8559a8b2594b90bb58cb2cf49
Angelillo15 <contact@angelillo15.es>
2024-07-12 10:51:54 +0200

    Add new Inventory to InventoryList

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/inventory/InventoryList.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/inventory/InventoryList.java
@@ -2,4 +2,5 @@ package com.nookure.staff.paper.inventory;
 
 public class InventoryList {
   public static final String STAFF_LIST = "staff/staff-list.peb";
+  public static final String PLAYER_LIST = "player-list/index.peb";
 }
