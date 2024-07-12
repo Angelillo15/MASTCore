b04d3e081534c7d142b2d25d1899ca158e48422a
Angelillo15 <contact@angelillo15.es>
2024-07-12 13:15:52 +0200

    Register new inventories

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/inventory/InventoryList.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/inventory/InventoryList.java
@@ -3,4 +3,6 @@ package com.nookure.staff.paper.inventory;
 public class InventoryList {
   public static final String STAFF_LIST = "staff/staff-list.peb";
   public static final String PLAYER_LIST = "player-list/index.peb";
+  public static final String NOTE_LIST = "notes/notes.peb";
+  public static final String PLAYER_ACTIONS = "player-list/actions.peb";
 }
