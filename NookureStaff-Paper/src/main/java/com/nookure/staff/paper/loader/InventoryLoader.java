9daf0358af08d53ad985da005d9b989b16df340f
Angelillo15 <contact@angelillo15.es>
2024-07-12 10:52:14 +0200

    Add the possibility to pass a flag to replace the UI files

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/loader/InventoryLoader.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/loader/InventoryLoader.java
@@ -1,10 +1,13 @@
 package com.nookure.staff.paper.loader;
 
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 import com.nookure.core.inv.paper.PaperNookureInventoryEngine;
+import com.nookure.core.inv.template.extension.OpenInventoryExtension;
 import com.nookure.core.inv.template.extension.PaginationItemExtension;
 import com.nookure.staff.api.util.AbstractLoader;
 import com.nookure.staff.api.util.JarUtil;
+import com.nookure.staff.paper.inventory.extenion.NookurePlayerExtension;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -15,18 +18,35 @@ public class InventoryLoader implements AbstractLoader {
   private JavaPlugin plugin;
   @Inject
   private AtomicReference<PaperNookureInventoryEngine> engine;
+  @Inject
+  private Injector injector;
+  private static final boolean replaceFolder;
+
+  static {
+    String property = System.getProperty("nkstaff.inventory.replace");
+
+    if (property == null) {
+      replaceFolder = false;
+    } else {
+      replaceFolder = Boolean.parseBoolean(property);
+    }
+  }
 
   @Override
   public void load() {
     PaperNookureInventoryEngine.Builder builder = new PaperNookureInventoryEngine.Builder()
         .plugin(plugin)
         .templateFolder("inventories")
-        .extensions(new PaginationItemExtension());
+        .extensions(new PaginationItemExtension(), new OpenInventoryExtension(), injector.getInstance(NookurePlayerExtension.class));
 
     engine.set(builder.build());
 
     try {
-      JarUtil.copyFolderFromJar("inventories", plugin.getDataFolder(), JarUtil.CopyOption.COPY_IF_NOT_EXIST);
+      JarUtil.CopyOption option;
+
+      option = replaceFolder ? JarUtil.CopyOption.REPLACE_IF_EXIST : JarUtil.CopyOption.COPY_IF_NOT_EXIST;
+
+      JarUtil.copyFolderFromJar("inventories", plugin.getDataFolder(), option);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
