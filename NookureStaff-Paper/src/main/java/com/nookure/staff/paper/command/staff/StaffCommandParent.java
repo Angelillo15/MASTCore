d7aaef5eb2ee309c927162d1add5528d13eb59eb
Angelillo15 <contact@angelillo15.es>
2024-07-15 14:43:55 +0200

    Add subcommands and create a custom annotation for staff no args command

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/command/staff/StaffCommandParent.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/command/staff/StaffCommandParent.java
@@ -2,11 +2,13 @@ package com.nookure.staff.paper.command.staff;
 
 import com.nookure.staff.api.Permissions;
 import com.nookure.staff.api.StaffPlayerWrapper;
+import com.nookure.staff.api.command.Command;
 import com.nookure.staff.api.command.CommandData;
 import com.nookure.staff.api.command.CommandParent;
 import com.nookure.staff.api.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.annotation.Annotation;
 import java.util.List;
 
 @CommandData(
@@ -14,7 +16,9 @@ import java.util.List;
     description = "Staff mode main command",
     permission = Permissions.STAFF_MODE_PERMISSION,
     subCommands = {
-      StaffListCommand.class
+        StaffListCommand.class,
+        StaffInvSee.class,
+        StaffEnderchestSee.class,
     }
 )
 public class StaffCommandParent extends CommandParent {
@@ -38,6 +42,41 @@ public class StaffCommandParent extends CommandParent {
     super.prepare();
     getSubCommandData().remove("help");
     getSubCommands().remove("help");
-    getSubCommandData().put(" ", getCommandData());
+    getSubCommandData().put(" ", new CommandData() {
+      @Override
+      public Class<? extends Annotation> annotationType() {
+        return CommandData.class;
+      }
+
+      @Override
+      public String name() {
+        return "";
+      }
+
+      @Override
+      public String[] aliases() {
+        return new String[0];
+      }
+
+      @Override
+      public String permission() {
+        return Permissions.STAFF_MODE_PERMISSION;
+      }
+
+      @Override
+      public String usage() {
+        return "/staff";
+      }
+
+      @Override
+      public String description() {
+        return "Enter in staff mode";
+      }
+
+      @Override
+      public Class<? extends Command>[] subCommands() {
+        return new Class[0];
+      }
+    });
   }
 }
