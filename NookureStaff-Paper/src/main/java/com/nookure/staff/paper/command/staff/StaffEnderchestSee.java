728a401243d687ae06b73b09342ffcef5e75d065
Angelillo15 <contact@angelillo15.es>
2024-07-15 14:43:36 +0200

    Create commands to modify enderchest & player inventory

---
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/command/staff/StaffEnderchestSee.java
@@ -0,0 +1,51 @@
+package com.nookure.staff.paper.command.staff;
+
+import com.google.inject.Inject;
+import com.nookure.staff.api.Permissions;
+import com.nookure.staff.api.StaffPlayerWrapper;
+import com.nookure.staff.api.command.CommandData;
+import com.nookure.staff.api.command.StaffCommand;
+import com.nookure.staff.api.config.ConfigurationContainer;
+import com.nookure.staff.api.config.bukkit.BukkitMessages;
+import com.nookure.staff.paper.StaffPaperPlayerWrapper;
+import com.nookure.staff.paper.inventory.player.PlayerEnderchestInventory;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+@CommandData(
+    name = "enderchest",
+    description = "View the enderchest inventory of another player",
+    permission = Permissions.STAFF_ENDERCHEST
+)
+public class StaffEnderchestSee extends StaffCommand {
+  @Inject
+  private ConfigurationContainer<BukkitMessages> messages;
+
+  @Override
+  protected void onStaffCommand(@NotNull StaffPlayerWrapper sender, @NotNull String label, @NotNull List<String> args) {
+    if (args.isEmpty()) {
+      sender.sendMiniMessage(messages.get().staffMode.invseeEnderchestCommandUsage());
+      return;
+    }
+
+    String targetName = args.getFirst();
+
+    Player target = Bukkit.getPlayer(targetName);
+
+    if (target == null) {
+      sender.sendMiniMessage(messages.get().playerNotFound());
+      return;
+    }
+
+    if (sender instanceof StaffPaperPlayerWrapper player) {
+      if (player.hasPermission(Permissions.STAFF_ENDERCHEST_MODIFY)) {
+        player.getPlayer().openInventory(target.getEnderChest());
+      } else {
+        player.getPlayer().openInventory(new PlayerEnderchestInventory(target).getInventory());
+      }
+    }
+  }
+}
