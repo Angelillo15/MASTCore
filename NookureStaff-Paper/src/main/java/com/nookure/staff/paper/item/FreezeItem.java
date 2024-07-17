1e8bf802f67636c7995f4798f020ea5dfffd0c27
Angelillo15 <contact@angelillo15.es>
2024-07-17 12:36:11 +0200

    Add permission check before freezing with the item

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/item/FreezeItem.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/item/FreezeItem.java
@@ -1,9 +1,11 @@
 package com.nookure.staff.paper.item;
 
 import com.google.inject.Inject;
+import com.nookure.staff.api.Permissions;
 import com.nookure.staff.api.PlayerWrapper;
 import com.nookure.staff.api.StaffPlayerWrapper;
 import com.nookure.staff.api.config.ConfigurationContainer;
+import com.nookure.staff.api.config.bukkit.BukkitMessages;
 import com.nookure.staff.api.config.bukkit.ItemsConfig;
 import com.nookure.staff.api.item.Items;
 import com.nookure.staff.api.item.PlayerInteractItem;
@@ -14,16 +16,27 @@ import org.jetbrains.annotations.NotNull;
 
 public class FreezeItem extends StaffItem implements PlayerInteractItem {
   private final FreezeManager freezeManager;
+  private final ConfigurationContainer<BukkitMessages> messages;
 
   @Inject
-  public FreezeItem(ConfigurationContainer<ItemsConfig> itemsConfig, FreezeManager freezeManager) {
+  public FreezeItem(
+      ConfigurationContainer<ItemsConfig> itemsConfig,
+      FreezeManager freezeManager,
+      ConfigurationContainer<BukkitMessages> messages
+  ) {
     super(itemsConfig.get().staffItems.getItems().get(Items.FREEZE.toString()));
     this.freezeManager = freezeManager;
+    this.messages = messages;
   }
 
   @Override
   public void click(@NotNull PlayerWrapper player, @NotNull PlayerWrapper target) {
     if (player instanceof StaffPlayerWrapper staff) {
+      if (target.hasPermission(Permissions.STAFF_FREEZE_BYPASS)) {
+        staff.sendMiniMessage(messages.get().freeze.freezeBypassMessage());
+        return;
+      }
+
       staff.getExtension(FreezePlayerExtension.class).ifPresent(freezePlayerExtension -> {
         if (freezeManager.isFrozen(target)) {
           freezePlayerExtension.unfreezePlayer(target);
