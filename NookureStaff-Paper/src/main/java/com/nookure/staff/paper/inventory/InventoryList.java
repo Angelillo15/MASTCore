527e8114eddcf9375561e831953e97172cec8110
Angelillo15 <contact@angelillo15.es>
2024-07-19 12:36:03 +0200

    Create enter-pin page

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/inventory/InventoryList.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/inventory/InventoryList.java
@@ -5,4 +5,5 @@ public class InventoryList {
   public static final String PLAYER_LIST = "player-list/index.peb";
   public static final String NOTE_LIST = "notes/notes.peb";
   public static final String PLAYER_ACTIONS = "player-list/actions.peb";
+  public static final String ENTER_PIN = "pin/enter-pin.peb";
 }
